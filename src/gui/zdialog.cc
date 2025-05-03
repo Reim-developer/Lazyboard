1eeb3f3cff7ecb2e80ec022b7eeb6948a162f5b2
Reim-developer <contact.kaxtr@gmail.com>
2025-05-03 10:35:17 +0700

    Add zDialog class to management dialog on application

---
+++ src/gui/zdialog.cc
@@ -0,0 +1,51 @@
+#include "include/zdialog.hpp"
+#include <QPointer>
+#include <QGridLayout>
+#include <QPushButton>
+#include <QIcon>
+#include <QPlainTextEdit>
+#include <QApplication>
+#include <QClipboard>
+#include <QTimer>
+
+using namespace zclipboard::zgui;
+
+void ZDialog::showZContentDialog(const QString &text, QTableWidget *ztableWidget) {
+    QDialog *zContentDialog = new QDialog(ztableWidget);
+    QIcon zIcon = QIcon(":/assets/assets/icon.png");
+    QGridLayout *zDialogLayout = new QGridLayout(zContentDialog);
+
+    QPlainTextEdit *zContentArea = new QPlainTextEdit();
+    QPushButton *zCopyButton = new QPushButton();
+
+    zContentDialog->setWindowTitle("zContent Clipboard");
+    zContentDialog->resize(600, 600);
+    zContentDialog->setWindowIcon(zIcon);
+
+    zContentArea->setReadOnly(true);
+    zContentArea->setPlainText(text);
+    zContentArea->setLineWrapMode(QPlainTextEdit::NoWrap);
+
+    zCopyButton->setText("Copy Content");
+    zCopyButton->setIcon(QIcon::fromTheme("edit-copy"));
+
+    QPointer<QPushButton> zSafeCopyButton = zCopyButton;
+    
+    connect(zCopyButton, &QPushButton::clicked, [zSafeCopyButton, text] {
+        QApplication::clipboard()->setText(text);
+        
+        if(zSafeCopyButton) {
+            zSafeCopyButton->setText("Copied!");
+
+            QTimer::singleShot(1500, [zSafeCopyButton]() {
+                if (zSafeCopyButton) zSafeCopyButton->setText("Copy Content");
+            });
+        }
+    });
+
+    zDialogLayout->addWidget(zCopyButton, 1, 0);
+    zDialogLayout->addWidget(zContentArea, 0, 0);
+
+    zContentDialog->setAttribute(Qt::WA_DeleteOnClose);
+    zContentDialog->exec();
+}
