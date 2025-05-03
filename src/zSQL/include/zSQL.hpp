9f1e7b141d749999bebea19f08b33f02daf65603
Reim-developer <contact.kaxtr@gmail.com>
2025-05-03 12:12:55 +0700

    Add method connectToDB executeQuery executeQueryResult
    to SQLite management on zSQL class

--- src/zSQL/include/zSQL.hpp
+++ src/zSQL/include/zSQL.hpp
@@ -1,9 +1,24 @@
 #ifndef zSQL_HPP
 #define zSQL_HPP
+#include <QString>
+#include <QVariantMap>
+#include <QtSql/QSqlDatabase>
 
 namespace zcipboard::zSQL {
     class zSQL {
-        void zInitCache();
+        public:
+            zSQL();
+
+        public:
+            void connectToDB();
+            void executeQuery(const QString &sql, 
+                              const QVariantMap &params = QVariantMap());
+            QSqlQuery executeQueryResult(const QString &sql, 
+                                         const QVariantMap &params = QVariantMap());
+
+        private:
+            QSqlDatabase zDB;
+            void zInitCache();
     };
 }
 
