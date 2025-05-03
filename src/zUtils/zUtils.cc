3d5eccb3720a91ecf8afe2466ba5c9bea659b1b7
Reim-developer <contact.kaxtr@gmail.com>
2025-05-03 11:11:21 +0700

    Add zUtils class with getCachePath function for platform-specific cache path retrieval

---
+++ src/zUtils/zUtils.cc
@@ -0,0 +1,13 @@
+#include "include/zUtils.hpp"
+#include <QtSystemDetection>
+#include <QStandardPaths>
+
+using namespace zclipboard::zUtils;
+
+QString zUtils::getCachePath() {
+    #ifdef Q_OS_WINDOWS
+        return QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation);
+    #else
+        return QStandardPaths::writableLocation(QStandardPaths::CacheLocation);
+    #endif
+}
