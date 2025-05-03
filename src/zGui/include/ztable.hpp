af74481e2cd2407a9ac077e17d9f3adc034470ff
Reim-developer <contact.kaxtr@gmail.com>
2025-05-03 11:11:09 +0700

    Add ZTable class for clipboard history management and integration with ZDialog

---
+++ src/zGui/include/ztable.hpp
@@ -0,0 +1,38 @@
+#ifndef ZTABLE_HPP 
+#define ZTABLE_HPP
+#include <QWidget>
+#include <QGridLayout>
+#include <QTableWidget>
+#include <QObject>
+#include <QClipboard>
+#include <QDialog>
+#include <QIcon>
+#include <QPlainTextEdit>
+#include <QGridLayout>
+#include <QPushButton>
+#include <QSet>
+#include <QString>
+#include "zdialog.hpp"
+#define CONTENT_COLUMN 1
+
+namespace zclipboard::zgui {
+    class ZTable : public QObject {
+        Q_OBJECT
+
+        public:
+            QTableWidget *ztableWidget;
+            void addZtable(QWidget *zWindow, QGridLayout *zLayout);
+
+        private:
+            QClipboard *zClipboard;
+            QSet<QString> zExistingContents;
+            ZDialog *zDialog;
+            
+            void addClipboardHistory();
+            
+        public slots:
+        void onContentClicked(QTableWidgetItem *ztableWidgetItem);
+    };
+} // namespace zclipboard::gui
+
+#endif 
