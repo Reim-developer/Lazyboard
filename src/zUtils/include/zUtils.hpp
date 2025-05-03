f213f458eb39c05569cc4223654ae14e5ef51f13
Reim-developer <contact.kaxtr@gmail.com>
2025-05-03 11:11:24 +0700

    Add zUtils class with getCachePath function for cache path retrieval

---
+++ src/zUtils/include/zUtils.hpp
@@ -0,0 +1,12 @@
+#ifndef Z_UTILS_HPP
+#define Z_UTILS_HPP
+#include <QString>
+
+namespace zclipboard::zUtils {
+    class zUtils {
+        public:
+            QString getCachePath();
+    };
+}
+
+#endif 
