0a080debc2fcdb7545fa888885fb8bb7753a642d
Reim-developer <contact.kaxtr@gmail.com>
2025-05-03 12:11:29 +0700

    Implement SQLite to Ztable

--- src/zGui/ztable.cc
+++ src/zGui/ztable.cc
@@ -7,15 +7,21 @@
 #include <QApplication>
 #include <QTableWidgetItem>
 #include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlQuery>
 #include <QTimer>
 #include <QPointer>
 
 using namespace zclipboard::zgui;
 
+ZTable::ZTable() {
+    zSQLManager.connectToDB();
+}
+
 void ZTable::addZtable(QWidget *zWindow, QGridLayout *zLayout) {
     ztableWidget = new QTableWidget(zWindow);
+    zClipboard = QApplication::clipboard();
+
     ztableWidget->setColumnCount(3);
-    
     ztableWidget->setHorizontalHeaderLabels({"Time", "Content", "Content Length"});
 
     ztableWidget->horizontalHeader()->setSectionResizeMode(0, QHeaderView::ResizeToContents);
@@ -25,7 +31,26 @@ void ZTable::addZtable(QWidget *zWindow, QGridLayout *zLayout) {
     ztableWidget->setEditTriggers(QAbstractItemView::NoEditTriggers);
     zLayout->addWidget(ztableWidget, 0, 0);
 
-    zClipboard = QApplication::clipboard();
+
+    QSqlQuery sqlQuery = zSQLManager.executeQueryResult(R"(
+        --sql
+        SELECT time, content, length FROM clipboard
+        ORDER BY time DESC
+    )");
+
+    while (sqlQuery.next()) {
+        QString time = sqlQuery.value(0).toString();
+        QString content = sqlQuery.value(1).toString();
+        int contentLength = sqlQuery.value(2).toInt();
+        
+        int row = ztableWidget->rowCount();
+        ztableWidget->insertRow(row);
+        ztableWidget->setItem(row, 0, new QTableWidgetItem(time));
+       
+        ztableWidget->setItem(row, 1, new QTableWidgetItem(content));   
+        ztableWidget->setItem(row, 2, new QTableWidgetItem(QString::number(contentLength)));
+    }
+
     connect(ztableWidget, &QTableWidget::itemClicked, this, &ZTable::onContentClicked);
     connect(zClipboard, &QClipboard::dataChanged, this, &ZTable::addClipboardHistory);
 }
@@ -47,6 +72,19 @@ void ZTable::addClipboardHistory() {
         ztableWidget->setItem(row, 2, new QTableWidgetItem(QString::number(textLength)));
 
         zExistingContents.insert(text);
+
+        QString insertSQL = R"(
+            --sql
+            INSERT INTO clipboard (time, content, length)
+            VALUES(:time, :content, :length)
+        )";
+
+        QVariantMap params;
+        params["time"] = time;
+        params["content"] = text;
+        params["length"] = textLength;
+
+        zSQLManager.executeQuery(insertSQL, params);
 }
 
 void ZTable::onContentClicked(QTableWidgetItem *ztableWidgetItem) {
