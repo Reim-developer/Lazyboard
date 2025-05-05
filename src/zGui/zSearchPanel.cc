f15d4e76db5bdf812ca7d28aa96afb6072abd684
Reim-developer <contact.kaxtr@gmail.com>
2025-05-06 00:21:41 +0700

    Add new application feature: + Search real-time in SearchPanel

--- src/zGui/zSearchPanel.cc
+++ src/zGui/zSearchPanel.cc
@@ -1,10 +1,17 @@
 #include "include/zSearchPanel.hpp"
 #include <QLineEdit>
+#include <QVariant>
+#include <QList>
+#include <QTimer>
 
 using zclipboard::zGui::SearchArea;
 
-void SearchArea::addSearchPanel(QWidget *zWindow, QGridLayout *zLayout) {
+void SearchArea::addSearchPanel(QWidget *zWindow, QGridLayout *zLayout, zTableModel *zModel) {
     zLineEdit = new QLineEdit(zWindow);
     zLineEdit->setPlaceholderText("Search...");
     zLayout->addWidget(zLineEdit, 0, 0);
+
+    connect(zLineEdit, &QLineEdit::textChanged, this, [zModel](const QString &text) {
+        QTimer::singleShot(300, zModel, [zModel, text]() { zModel->filterItems(text); });
+    });
 }
