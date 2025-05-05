ab774be699d7e38ed4ae9523cee96901ac41efd4
Reim-developer <contact.kaxtr@gmail.com>
2025-05-05 21:36:30 +0700

    Refactor, add macro Z_DB_NAME, use unique_ptr<QSqlQuery>

--- src/zSQL/include/zSQL.hpp
+++ src/zSQL/include/zSQL.hpp
@@ -3,23 +3,25 @@
 #include <QString>
 #include <QVariantMap>
 #include <QtSql/QSqlDatabase>
+#include <memory>
+#define Z_DB_NAME "ZClipboardDB"
+
+using std::unique_ptr;
 
 namespace zclipboard::zSQL {
-    class zManagerSQL {
-        public:
-            zManagerSQL();
+class zManagerSQL {
+   public:
+    zManagerSQL();
 
-        public:
-            void connectToDB();
-            void executeQuery(const QString &sql, 
-                              const QVariantMap &params = QVariantMap());
-            QSqlQuery executeQueryResult(const QString &sql, 
-                                         const QVariantMap &params = QVariantMap());
+   public:
+    void setupinitDB();
+    void executeQuery(const QString &sql, const QVariantMap &params = QVariantMap());
+    unique_ptr<QSqlQuery> executeQueryResult(const QString &sql, const QVariantMap &params = QVariantMap());
 
-        private:
-            QSqlDatabase zDB;
-            void zInitCache();
-    };
-}
+   private:
+    QSqlDatabase zDB;
+    void zInitCache();
+};
+}  // namespace zclipboard::zSQL
 
-#endif 
+#endif
