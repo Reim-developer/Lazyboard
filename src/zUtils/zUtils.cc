8a15d19a9368adea8acc61a769767ce4afa55a26
Reim-developer <contact.kaxtr@gmail.com>
2025-05-06 02:14:40 +0700

    Remove private header <QtSystemDetection> to utils

--- src/zUtils/zUtils.cc
+++ src/zUtils/zUtils.cc
@@ -1,13 +1,13 @@
 #include "include/zUtils.hpp"
-#include <QtSystemDetection>
+#include <QtCoreGlobal>
 #include <QStandardPaths>
 
 using namespace zclipboard;
 
 QString zUtils::getCachePath() {
-    #ifdef Q_OS_WINDOWS
-        return QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation);
-    #else
-        return QStandardPaths::writableLocation(QStandardPaths::CacheLocation);
-    #endif
+#ifdef Q_OS_WIN
+    return QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation);
+#else
+    return QStandardPaths::writableLocation(QStandardPaths::CacheLocation);
+#endif
 }
