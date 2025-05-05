1bb80f0a6abac5177ca95fa1cb66c66efda9fa32
Reim-developer <contact.kaxtr@gmail.com>
2025-05-06 00:25:37 +0700

    Re-format codebase to Google style. For more information, please check .clang-format

--- src/zGui/include/zSearchPanel.hpp
+++ src/zGui/include/zSearchPanel.hpp
@@ -1,20 +1,24 @@
 #ifndef zSearchPanel_HPP
 #define zSearchPanel_HPP
 #include <QObject>
+#include <QString>
 #include <QWidget>
 #include <QLineEdit>
 #include <QGridLayout>
+#include "zClipboardModel.hpp"
+
+using zclipboard::zGui::zTableModel;
 
 namespace zclipboard::zGui {
-    class SearchArea : public QObject {
-        Q_OBJECT
-        
-        public:
-            void addSearchPanel(QWidget *zWindow, QGridLayout *zLayout);
+class SearchArea : public QObject {
+    Q_OBJECT
 
-        private:
-            QLineEdit *zLineEdit;
-    };
-}
+   public:
+    void addSearchPanel(QWidget *zWindow, QGridLayout *zLayout, zTableModel *zModel);
 
-#endif // zSearchPanel_HPP
+   private:
+    QLineEdit *zLineEdit;
+};
+}  // namespace zclipboard::zGui
+
+#endif  // zSearchPanel_HPP
