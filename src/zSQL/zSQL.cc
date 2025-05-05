6f9f4151e78e06a90c476afe529f08c3ffc3d1de
Reim-developer <contact.kaxtr@gmail.com>
2025-05-05 21:36:47 +0700

    Apply change, refactor in zSQL.hpp header file

--- src/zSQL/zSQL.cc
+++ src/zSQL/zSQL.cc
@@ -1,41 +1,51 @@
 #include "include/zSQL.hpp"
 #include "../zUtils/include/zUtils.hpp"
+#include <QDir>
+#include <QFileInfo>
 #include <QString>
 #include <QVariantMap>
 #include <QtSql/QSqlQuery>
-#include <QDir>
+#include <memory>
 
-using namespace zclipboard::zSQL;
+using zclipboard::zSQL::zManagerSQL;
 using namespace zclipboard;
+using std::make_unique;
 
 zManagerSQL::zManagerSQL() {
     zInitCache();
 }
 
-
 void zManagerSQL::zInitCache() {
     QDir qDir;
     QString cachePath = zUtils::getCachePath();
 
-    if(!qDir.exists(cachePath)) qDir.mkdir(cachePath);
+    if (!qDir.exists(cachePath)) qDir.mkdir(cachePath);
 }
 
-void zManagerSQL::connectToDB() {
+void zManagerSQL::setupinitDB() {
     QString dbPath = zUtils::getCachePath() + "/zclipboard.db";
+    QDir directory;
 
-    zDB = QSqlDatabase::addDatabase("QSQLITE", "ZClipboardDB");
-    zDB.setDatabaseName(dbPath);
-    zDB.open();
+    if (!directory.exists(dbPath)) directory.mkpath(zUtils::getCachePath());
+
+    if (!QSqlDatabase::contains(Z_DB_NAME)) {
+        zDB = QSqlDatabase::addDatabase("QSQLITE", Z_DB_NAME);
+        zDB.setDatabaseName(dbPath);
+    } else {
+        zDB = QSqlDatabase::database(Z_DB_NAME);
+    }
+
+    if (!zDB.isOpen()) zDB.open();
 
     QSqlQuery sqlQuery(zDB);
     sqlQuery.exec(R"(
-        --sql
-        CREATE TABLE IF NOT EXISTS clipboard (
-            time TEXT, content TEXT,
-            content_hash TEXT PRIMARY KEY,
-            length INTEGER,
-            image_data BLOB
-        )    
+            --sql
+            CREATE TABLE IF NOT EXISTS clipboard (
+                time TEXT, content TEXT,
+                content_hash TEXT PRIMARY KEY,
+                length INTEGER,
+                image_data BLOB
+            )    
     )");
 }
 
@@ -43,20 +53,18 @@ void zManagerSQL::executeQuery(const QString &sql, const QVariantMap &params) {
     QSqlQuery sqlQuery(zDB);
     sqlQuery.prepare(sql);
 
-    for(auto it = params.begin(); it != params.end(); it++)
-        sqlQuery.bindValue(":" + it.key(), it.value());
+    for (auto it = params.begin(); it != params.end(); it++) sqlQuery.bindValue(":" + it.key(), it.value());
 
     sqlQuery.exec();
 }
 
-QSqlQuery zManagerSQL::executeQueryResult(const QString &sql, const QVariantMap &params) {
-    QSqlQuery sqlQuery(zDB);
-    sqlQuery.prepare(sql);
+unique_ptr<QSqlQuery> zManagerSQL::executeQueryResult(const QString &sql, const QVariantMap &params) {
+    auto sqlQuery = make_unique<QSqlQuery>(zDB);
+    sqlQuery->prepare(sql);
 
-    for(auto it = params.begin(); it != params.end(); it++)
-        sqlQuery.bindValue(":"+ it.key(), it.value());
+    for (auto it = params.begin(); it != params.end(); it++) sqlQuery->bindValue(":" + it.key(), it.value());
 
-    sqlQuery.exec();
+    sqlQuery->exec();
 
     return sqlQuery;
 }
