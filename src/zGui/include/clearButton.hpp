f1fed100b5b6a7cb036400c953b049cb143813ff
Reim-developer <contact.kaxtr@gmail.com>
2025-05-06 11:28:44 +0700

    Define + Clear Button UI Widget to clear all content clipboard

---
+++ src/zGui/include/clearButton.hpp
@@ -0,0 +1,21 @@
+#ifndef CLEAR_BUTTON_HPP
+#define CLEAR_BUTTON_HPP
+#include <QGridLayout>
+#include <QPushButton>
+#include <QObject>
+#include "ztable.hpp"
+
+using zclipboard::zGui::ZTable;
+
+namespace zclipboard::zGui {
+class ClearButton : public QObject {
+    Q_OBJECT
+   public:
+    void addClearButton(QGridLayout *layout, ZTable *table);
+
+   private:
+    QPushButton *clearButton;
+};
+}  // namespace zclipboard::zGui
+
+#endif  // CLEAR_BUTTON_HPP
