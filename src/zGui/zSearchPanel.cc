9c8661a04bb0f90035f99fae328c3359b682a901
Reim-developer <contact.kaxtr@gmail.com>
2025-05-06 11:29:27 +0700

    Implement refactor in zSearchPanel.hpp: + Remove zTableModel parameter, use ZTable instead

--- src/zGui/zSearchPanel.cc
+++ src/zGui/zSearchPanel.cc
@@ -3,14 +3,16 @@
 #include <QVariant>
 #include <QList>
 #include <QTimer>
+#include "include/ztable.hpp"
 
 using zclipboard::zGui::SearchArea;
 
-void SearchArea::addSearchPanel(QWidget *zWindow, QGridLayout *zLayout, zTableModel *zModel) {
+void SearchArea::addSearchPanel(QWidget *zWindow, QGridLayout *zLayout, ZTable *table) {
     zLineEdit = new QLineEdit(zWindow);
     zLineEdit->setPlaceholderText("Search...");
     zLayout->addWidget(zLineEdit, 0, 0);
 
+    auto zModel = table->getZModel();
     connect(zLineEdit, &QLineEdit::textChanged, this, [zModel](const QString &text) {
         QTimer::singleShot(300, zModel, [zModel, text]() { zModel->filterItems(text); });
     });
