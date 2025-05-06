72f10696212f9b423feee3e43d1e8ccebf42b743
Reim-developer <contact.kaxtr@gmail.com>
2025-05-06 23:47:46 +0700

    Define column Pin in TableModel Widget

--- src/zGui/include/zClipboardModel.hpp
+++ src/zGui/include/zClipboardModel.hpp
@@ -5,6 +5,9 @@
 #include <QString>
 #include <QByteArray>
 #include <QList>
+#include "../../zSQL/include/zSQL.hpp"
+
+using zclipboard::zSQL::zManagerSQL;
 
 namespace zclipboard::zGui {
 struct zClipboardItem {
@@ -13,24 +16,31 @@ struct zClipboardItem {
     int contentLength;
     QByteArray imageData;
     QString hash;
+    bool isPinned = false;
 };
 
 class zTableModel : public QAbstractTableModel {
     Q_OBJECT
 
    public:
-    enum Column { Time = 0, Content = 1, ContentLength = 2, ColumnCount = 3 };
+    enum Column { Time = 0, Content = 1, ContentLength = 2, Pin = 3, ColumnCount = 4 };
 
-    explicit zTableModel(QObject *parent = nullptr);
+    explicit zTableModel(zManagerSQL &zSQL, QObject *parent = nullptr);
     ~zTableModel();
 
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     int columnCount(const QModelIndex &parent = QModelIndex()) const override;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+    QVariant headerData(int section, Qt::Orientation orientation,
+                        int role = Qt::DisplayRole) const override;
 
-    void addTextItem(const QString &time, const QString &text, const QString &hash, int length);
-    void addImageItem(const QString &time, const QString &hash, const QByteArray &imageData);
+    Qt::ItemFlags flags(const QModelIndex &index) const override;
+    bool setData(const QModelIndex &index, const QVariant &value, int role) override;
+
+    void addTextItem(const QString &time, const QString &text, const QString &hash, int length,
+                     bool isPinned = false);
+    void addImageItem(const QString &time, const QString &hash, const QByteArray &imageData,
+                      bool isPinned = false);
     void clearData();
     void filterItems(const QString &searchText);
 
@@ -39,6 +49,8 @@ class zTableModel : public QAbstractTableModel {
     QList<zClipboardItem> m_items;
     QList<zClipboardItem> mData;
     QSet<QString> m_existingHashes;
+
+    zManagerSQL &m_SqlManager;
 };
 }  // namespace zclipboard::zGui
 
