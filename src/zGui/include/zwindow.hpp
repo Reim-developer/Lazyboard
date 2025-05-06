5b90476ea60ba3a3a465824e7f934f2d673d19a0
Reim-developer <contact.kaxtr@gmail.com>
2025-05-06 11:30:59 +0700

    Define UI Widget: Clear Button

--- src/zGui/include/zwindow.hpp
+++ src/zGui/include/zwindow.hpp
@@ -6,26 +6,30 @@
 #include <QWidget>
 #include "ztable.hpp"
 #include "zSearchPanel.hpp"
+#include "clearButton.hpp"
 
+using zclipboard::zGui::ClearButton;
 using zclipboard::zGui::SearchArea;
 
 namespace zclipboard::zGui {
-    class ZWindow : public QMainWindow {
-        Q_OBJECT
+class ZWindow : public QMainWindow {
+    Q_OBJECT
 
-        public:
-            explicit ZWindow(QWidget *zWindow = nullptr);
-        
-        private:
-            void setupGui();
-            
-        private:
-            QIcon zIcon;
-            QWidget *zCentralWidget;
-            QGridLayout *zLayout;
-            ZTable *ztable;
-            SearchArea *zSearchArea;
-    };
-} // namespace zlipboard::zgui
+   public:
+    explicit ZWindow(QWidget *zWindow = nullptr);
 
-#endif // ZWINDOW_HPP
+   private:
+    void setupGui();
+
+   private:
+    QIcon zIcon;
+    QWidget *zCentralWidget;
+    QGridLayout *zLayout;
+
+    ZTable *ztable;
+    SearchArea *zSearchArea;
+    ClearButton *clearButton;
+};
+}  // namespace zclipboard::zGui
+
+#endif  // ZWINDOW_HPP
