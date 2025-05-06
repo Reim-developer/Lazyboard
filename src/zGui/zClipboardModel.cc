fe2e533e2789b3b85c085e81c9d6224561b17af4
Reim-developer <contact.kaxtr@gmail.com>
2025-05-06 23:46:56 +0700

    Create instance zManagerSQL& to constructor ZTableMOdel

--- src/zGui/zClipboardModel.cc
+++ src/zGui/zClipboardModel.cc
@@ -1,10 +1,13 @@
 #include "include/zClipboardModel.hpp"
+
 #include <QImage>
 #include <QPixmap>
 
 using zclipboard::zGui::zTableModel;
+using zclipboard::zSQL::zManagerSQL;
 
-zTableModel::zTableModel(QObject *parent) : QAbstractTableModel(parent) {}
+zTableModel::zTableModel(zManagerSQL &zSQL, QObject *parent)
+    : QAbstractTableModel(parent), m_SqlManager(zSQL) {}
 
 zTableModel::~zTableModel() {}
 
@@ -28,9 +31,11 @@ QVariant zTableModel::data(const QModelIndex &index, int role) const {
 
             case Content:
                 if (!item.imageData.isEmpty()) return QVariant();
+
                 if (item.content.length() > 20) {
                     int remainingLength = item.content.length() - 20;
-                    QString truncated = item.content.left(20) + QString(" and %1 more...").arg(remainingLength);
+                    QString truncated =
+                        item.content.left(20) + QString(" and %1 more...").arg(remainingLength);
 
                     return truncated;
                 }
@@ -39,15 +44,25 @@ QVariant zTableModel::data(const QModelIndex &index, int role) const {
 
             case ContentLength:
                 return item.contentLength;
+
+            case Pin:
+                return QVariant();
+
             default:
                 return QVariant();
         }
-    } else if (role == Qt::DecorationRole && index.column() == Content && !item.imageData.isEmpty()) {
+    }
+    if (role == Qt::DecorationRole && index.column() == Content && !item.imageData.isEmpty()) {
         QImage image;
         image.loadFromData(item.imageData, "PNG");
+
         return QPixmap::fromImage(image).scaled(128, 128, Qt::KeepAspectRatio);
-    } else if (role == Qt::UserRole && index.column() == Content)
-        return item.hash;
+    }
+
+    if (role == Qt::UserRole && index.column() == Content) return item.hash;
+
+    if (role == Qt::CheckStateRole && index.column() == Pin)
+        return item.isPinned ? Qt::Checked : Qt::Unchecked;
 
     return QVariant();
 }
@@ -61,22 +76,26 @@ QVariant zTableModel::headerData(int section, Qt::Orientation orientation, int r
                 return "Content  | Click to view";
             case ContentLength:
                 return "Content Length";
+            case Pin:
+                return "Pin";
         }
     }
 
     return QAbstractTableModel::headerData(section, orientation, role);
 }
 
-void zTableModel::addTextItem(const QString &time, const QString &text, const QString &hash, int length) {
+void zTableModel::addTextItem(const QString &time, const QString &text, const QString &hash,
+                              int length, bool isPinned) {
     if (m_existingHashes.contains(hash)) return;
 
-    beginInsertColumns(QModelIndex(), rowCount(), rowCount());
+    beginInsertRows(QModelIndex(), rowCount(), rowCount());
 
     zClipboardItem clipboardItem;
     clipboardItem.time = time;
     clipboardItem.content = text;
     clipboardItem.contentLength = length;
     clipboardItem.hash = hash;
+    clipboardItem.isPinned = isPinned;
 
     m_items.append(clipboardItem);
     mData.append(clipboardItem);
@@ -85,10 +104,11 @@ void zTableModel::addTextItem(const QString &time, const QString &text, const QS
     endInsertRows();
 }
 
-void zTableModel::addImageItem(const QString &time, const QString &hash, const QByteArray &imageData) {
+void zTableModel::addImageItem(const QString &time, const QString &hash,
+                               const QByteArray &imageData, bool isPinned) {
     if (m_existingHashes.contains(hash)) return;
 
-    beginInsertColumns(QModelIndex(), rowCount(), rowCount());
+    beginInsertRows(QModelIndex(), rowCount(), rowCount());
 
     zClipboardItem clipboardItem;
     clipboardItem.time = time;
@@ -96,6 +116,7 @@ void zTableModel::addImageItem(const QString &time, const QString &hash, const Q
     clipboardItem.contentLength = imageData.size();
     clipboardItem.imageData = imageData;
     clipboardItem.hash = hash;
+    clipboardItem.isPinned = isPinned;
 
     m_items.append(clipboardItem);
     mData.append(clipboardItem);
@@ -116,10 +137,35 @@ void zTableModel::filterItems(const QString &searchText) {
     QList<zClipboardItem> filteredData;
 
     for (const auto &item : m_items) {
-        if (item.content.contains(searchText) || item.hash.contains(searchText)) filteredData.append(item);
+        if (item.content.contains(searchText) || item.hash.contains(searchText))
+            filteredData.append(item);
     }
 
     beginResetModel();
     mData = filteredData;
     endResetModel();
+}
+
+Qt::ItemFlags zTableModel::flags(const QModelIndex &index) const {
+    Qt::ItemFlags flags = QAbstractItemModel::flags(index);
+
+    if (index.column() == Pin) flags |= Qt::ItemIsUserCheckable;
+
+    return flags;
+}
+
+bool zTableModel::setData(const QModelIndex &index, const QVariant &value, int role) {
+    if (!index.isValid()) return false;
+
+    if (role == Qt::CheckStateRole && index.column() == Pin) {
+        zClipboardItem &item = mData[index.row()];
+        item.isPinned = (value.toInt() == Qt::Checked);
+
+        m_SqlManager.updatePinStatus(item.hash, item.isPinned);
+
+        emit dataChanged(index, index, {Qt::CheckStateRole});
+        return true;
+    }
+
+    return false;
 }
