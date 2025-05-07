274a66de1aeb318fc6c20b4c54daedd4623b9fb0
Reim-developer <contact.kaxtr@gmail.com>
2025-05-08 01:08:05 +0700

    Add script build for macOS

---
+++ scripts/ci_macos.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+build_entry="build"
+opt_flags="-O3 -march=native -flto -funroll-loops -fomit-frame-pointer -fstrict-aliasing -ftree-vectorize -fvisibility=hidden"
+
+install_homebrew() {
+    if ! command -v brew > /dev/null; then 
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+}
+
+setup_env() {
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+}
+
+install_dependencies() {
+    brew install qt@6 cmake ninja
+
+    export PATH="/opt/homebrew/opt/qt@6/bin:$PATH"
+    export QT_DIR="/opt/homebrew/opt/qt@6"
+}
+
+create_dir() {
+    mkdir -p "$build_entry"
+    cd "$build_entry" || exit
+}
+
+cfg_and_build() {
+    cmake \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_CXX_COMPILER=clang++ \
+        -DCMAKE_CXX_FLAGS="$opt_flags" \
+        -G "Ninja" \
+        ..
+
+    ninja
+}
+
+match_options() {
+    case $1 in 
+    "install-base") install_homebrew ;;
+    "install-libs") install_dependencies ;;
+    "setup-build") create_build_dir ;;
+    "build-release") cfg_and_build ;;
+
+    *) echo "Missing arg: $1" && exit 1 ;;
+    esac
+}
+
+match_options "$1"
