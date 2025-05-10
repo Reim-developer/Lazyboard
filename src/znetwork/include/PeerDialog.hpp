d96c4adcd8146e25ab4744ad51f7e8009af2eae1
Reim-developer <contact.kaxtr@gmail.com>
2025-05-10 18:20:17 +0700

    Add PeerDialog class for device selection interface

---
+++ src/znetwork/include/PeerDialog.hpp
@@ -0,0 +1,29 @@
+#ifndef PEER_DIALOG_HPP
+#define PEER_DIALOG_HPP
+#include <QDialog>
+#include <QListWidget>
+#include <QPushButton>
+#include <QGridLayout>
+#include <QLabel>
+
+namespace zclipboard::znetwork {
+class PeerDialog : public QDialog {
+    Q_OBJECT
+
+   public:
+    explicit PeerDialog(QWidget *parent = nullptr);
+    QListWidget *getPeerList() const;
+
+   private:
+    QListWidget *peerList;
+    QPushButton *sendButton;
+    QGridLayout *layout;
+    QLabel *infoLabel;
+
+   private:
+    static inline constexpr int DIALOG_WIDTH_BASE = 600;
+    static inline constexpr int DIALOG_HEIGHT_BASE = 600;
+};
+}  // namespace zclipboard::znetwork
+
+#endif  // PEER_DIALOG_HPP
