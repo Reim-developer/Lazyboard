6f0b00d6951246654243323e19cb9bebbba0d7a9
Reim-developer <contact.kaxtr@gmail.com>
2025-05-10 18:20:20 +0700

    Add PeerDiscovery class for multicast peer discovery

---
+++ src/znetwork/include/PeerDiscovery.hpp
@@ -0,0 +1,33 @@
+#ifndef PEER_DISCOVERY_HPP
+#define PEER_DISCOVERY_HPP
+#include <QObject>
+#include <QtTypes>
+#include <QtNetwork/QUdpSocket>
+#include <QStringLiteral>
+
+namespace zclipboard::znetwork {
+class PeerDiscovery : public QObject {
+    Q_OBJECT
+
+   public:
+    explicit PeerDiscovery(quint16 port = 45454, QObject *parent = nullptr);
+
+   signals:
+    void peerFound(const QString &ipAddress);
+
+   private slots:
+    void broadcastHello();
+    void handleIncomingMessage();
+    void prepareDatagram();
+    QString getLocalDeviceName() const;
+    QString getLocalIPAddress() const;
+
+   private:
+    QString broadcast_msg;
+    QUdpSocket socket;
+    QHostAddress multicastGroup;
+    quint16 port;
+};
+}  // namespace zclipboard::znetwork
+
+#endif  // PEER_DISCOVERY_HPP
