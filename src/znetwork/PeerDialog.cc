5a552791a36551fb482405f03dd58220fb1bf94a
Reim-developer <contact.kaxtr@gmail.com>
2025-05-11 18:21:54 +0700

    Refactor PeerDialog to use QStringLiteral for UI text and add empty state handling

--- src/znetwork/PeerDialog.cc
+++ src/znetwork/PeerDialog.cc
@@ -12,21 +12,50 @@ PeerDialog::PeerDialog(QWidget *parent) : QDialog(parent) {
     sendButton = new QPushButton(this);
     layout = new QGridLayout(this);
     infoLabel = new QLabel(this);
+    emptyLabel = new QLabel(this);
 
     peerList->setSelectionMode(QAbstractItemView::SingleSelection);
-    sendButton->setText("Send To Selected Device");
-    infoLabel->setText("Devices in LAN:");
+    sendButton->setText(QStringLiteral("Send to selected device"));
+    sendButton->setEnabled(!peerList->selectedItems().isEmpty());
+
+    infoLabel->setText(QStringLiteral("Devices in LAN:"));
+
+    emptyLabel->setText(QStringLiteral(R"(
+        Nothing to show. Make sure device you want send clipboard
+        content already install ZClipboard and they want get
+        your clipboard content.
+    )"));
+    emptyLabel->setWordWrap(true);
+    emptyLabel->setAlignment(Qt::AlignCenter);
+    emptyLabel->hide();
 
     layout->addWidget(infoLabel, 0, 0);
     layout->addWidget(peerList, 1, 0);
+    layout->addWidget(emptyLabel, 1, 0);
     layout->addWidget(sendButton, 2, 0);
 
     connect(peerList, &QListWidget::itemSelectionChanged,
             [this]() { sendButton->setEnabled(!peerList->selectedItems().isEmpty()); });
 
+    connect(peerList->model(), &QAbstractItemModel::rowsInserted, this,
+            &PeerDialog::updateEmptyState);
+    connect(peerList->model(), &QAbstractItemModel::rowsRemoved, this,
+            &PeerDialog::updateEmptyState);
+
     setLayout(layout);
+    updateEmptyState();
 }
 
 QListWidget *PeerDialog::getPeerList() const {
     return peerList;
+}
+
+void PeerDialog::updateEmptyState() {
+    if (peerList->count() == 0) {
+        peerList->hide();
+        emptyLabel->show();
+    } else {
+        peerList->show();
+        emptyLabel->hide();
+    }
 }
