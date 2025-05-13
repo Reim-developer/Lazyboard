4e2f581f0514962aa4c13460133d312c6019ca46
Reim-developer <contact.kaxtr@gmail.com>
2025-05-13 18:41:35 +0700

    Add tray icon functionality to ZWindow for improved user interaction

--- src/zGui/zwindow.cc
+++ src/zGui/zwindow.cc
@@ -1,9 +1,11 @@
 #include "include/zwindow.hpp"
 #include "include/clearButton.hpp"
 #include "include/getButton.hpp"
+#include "include/settingButton.hpp"
 #include "include/ztable.hpp"
 #include "include/zSearchPanel.hpp"
 #include <QStringLiteral>
+#include <QApplication>
 
 using zclipboard::zGui::GetButton;
 using zclipboard::zGui::SearchArea;
@@ -22,6 +24,36 @@ ZWindow::ZWindow(QWidget *zWindow) : QMainWindow(zWindow) {
     resize(Z_WINDOW_WIDTH, Z_WINDOW_HEIGHT);
     setWindowIcon(zIcon);
     setupGui();
+    createTrayIcon();
+}
+
+void ZWindow::createTrayIcon() {
+    trayIcon = new QSystemTrayIcon(this);
+    trayMenu = new QMenu();
+
+    QAction *showGui = trayMenu->addAction(QStringLiteral("Show ZClipboard"));
+    QAction *quitGui = trayMenu->addAction(QStringLiteral("Quit ZClipboard"));
+    QAction *hideGui = trayMenu->addAction(QStringLiteral("Hide ZClipboard"));
+
+    addTrayMenuActions(trayIcon);
+    connect(showGui, &QAction::triggered, this, &ZWindow::showNormal);
+    connect(quitGui, &QAction::triggered, this, &QApplication::quit);
+    connect(hideGui, &QAction::triggered, this, &ZWindow::hide);
+
+    trayIcon->setIcon(zIcon);
+    trayIcon->setToolTip(QStringLiteral("Manage your clipboard - ZClipboard"));
+    trayIcon->setContextMenu(trayMenu);
+    trayIcon->show();
+}
+
+void ZWindow::addTrayMenuActions(QSystemTrayIcon *trayIcon) {
+    connect(trayIcon, &QSystemTrayIcon::activated,
+            [this](QSystemTrayIcon::ActivationReason reason) {
+                if (reason == QSystemTrayIcon::DoubleClick || reason == QSystemTrayIcon::Trigger) {
+                    showNormal();
+                    activateWindow();
+                }
+            });
 }
 
 void ZWindow::setupGui() {
@@ -29,9 +61,11 @@ void ZWindow::setupGui() {
     zSearchArea = new SearchArea();
     clearButton = new ClearButton();
     getButton = new GetButton();
+    settingButton = new SettingButton();
 
     ztable->addZtable(this, zLayout);
     zSearchArea->addSearchPanel(this, zLayout, ztable);
     clearButton->addClearButton(zLayout, ztable);
     getButton->addGetButton(this, zLayout);
+    settingButton->addSettingButton(this, zLayout);
 }
