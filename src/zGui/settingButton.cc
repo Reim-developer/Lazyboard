8f80c4e09b31a5e576b04eddcfaf3b3ef5ee508e
Reim-developer <contact.kaxtr@gmail.com>
2025-05-14 14:50:50 +0700

    Implement settings dialog in SettingButton with auto-hide and notification options

--- src/zGui/settingButton.cc
+++ src/zGui/settingButton.cc
@@ -1,5 +1,7 @@
 #include "include/settingButton.hpp"
+#include "../zUtils/include/settings.hpp"
 #include <QStringLiteral>
+#include <QSettings>
 #include <QIcon>
 
 using zclipboard::zGui::SettingButton;
@@ -17,6 +19,8 @@ void SettingButton::addSettingButton(QWidget *window, QGridLayout *layout) {
 
 void SettingButton::showSettingDialog(QWidget *parent) {
     QDialog *dialog = new QDialog(parent);
+    layout = new QGridLayout(dialog);
+
     QIcon icon = QIcon(QStringLiteral(":/assets/assets/icon.png"));
     constexpr int FIXED_WIDTH = 400;
     constexpr int FIXED_HEIGHT = 400;
@@ -24,5 +28,39 @@ void SettingButton::showSettingDialog(QWidget *parent) {
     dialog->setWindowTitle(QStringLiteral("ZClipboard - Setting"));
     dialog->setFixedSize(FIXED_WIDTH, FIXED_HEIGHT);
     dialog->setWindowIcon(icon);
+    dialog->setLayout(layout);
+    addGui(layout);
     dialog->exec();
+}
+
+void SettingButton::addGui(QGridLayout *layout) {
+    QSettings *settings = new QSettings();
+    autoHideCheckBox = new QCheckBox();
+    autoNotificatonCheckBox = new QCheckBox();
+
+    bool isHide = settings->value(AUTO_HIDE_SETTING).toBool();
+    bool isNotification = settings->value(AUTO_NOTIFICATION_SETTING).toBool();
+
+    autoHideCheckBox->setText(QStringLiteral("Minimize to system tray on window close."));
+    autoNotificatonCheckBox->setText(QStringLiteral("Notify me when clipboard changes."));
+
+    autoHideCheckBox->setChecked(isHide);
+    autoNotificatonCheckBox->setChecked(isNotification);
+    addHideSetting(autoHideCheckBox, settings);
+    addNotificationSetting(autoNotificatonCheckBox, settings);
+
+    layout->addWidget(autoHideCheckBox, 0, 0);
+    layout->addWidget(autoNotificatonCheckBox, 1, 0);
+}
+
+void SettingButton::addHideSetting(QCheckBox *autoHideCheckBox, QSettings *settings) {
+    connect(autoHideCheckBox, &QCheckBox::toggled,
+            [this, settings](bool isChecked) { settings->setValue(AUTO_HIDE_SETTING, isChecked); });
+}
+
+void SettingButton::addNotificationSetting(QCheckBox *autoNotificatonCheckBox,
+                                           QSettings *settings) {
+    connect(autoNotificatonCheckBox, &QCheckBox::toggled, [this, settings](bool isChecked) {
+        settings->setValue(AUTO_NOTIFICATION_SETTING, isChecked);
+    });
 }
