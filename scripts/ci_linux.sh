31d252db3ff149b8d1085aade31685f5736d4641
Reim-developer <contact.kaxtr@gmail.com>
2025-05-14 15:18:45 +0700

    Fix install_base function to correctly include 'install' in apt command

--- scripts/ci_linux.sh
+++ scripts/ci_linux.sh
@@ -11,7 +11,7 @@ create_build_dir() {
 }
 
 install_base() {
-    sudo apt git python3 perl install build-essential clang cmake ninja-build \
+    sudo apt install git python3 perl install build-essential clang cmake ninja-build \
     libgl1-mesa-dev libx11-dev libxext-dev libxrender-dev \
     libxcb1-dev libx11-xcb-dev libglu1-mesa-dev
 }
