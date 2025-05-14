7074f71b8269dda4136c229d332e16bbea49590f
Reim-developer <contact.kaxtr@gmail.com>
2025-05-14 15:02:10 +0700

    Refactor CI script to streamline Qt static build process and update package installations

--- scripts/ci_linux.sh
+++ scripts/ci_linux.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 build_dir="build"
 release_flags="-DCMAKE_BUILD_TYPE=Release"
+qt_static_dir="$build_dir/qt-static"
+qt_src_dir="$build_dir/qt-src"
 opt_flags="-O3 -march=native -flto -funroll-loops -fomit-frame-pointer -fstrict-aliasing -ftree-vectorize -fvisibility=hidden"
 nproc=$(nproc)
 
@@ -9,37 +11,64 @@ create_build_dir() {
 }
 
 install_base() {
-    sudo apt  install -y \
-                clang \
-                qt6-base-dev \
-                qt6-base-private-dev \
-                ninja-build \
-                cmake \
-                build-essential
+     sudo apt install -y \
+        git \
+        clang \
+        cmake \
+        ninja-build \
+        build-essential \
+        libgl1 \
+        python3 \
+        patchelf \
+        wget \
+        perl
+}
+
+build_qt_static() {
+    cd "$qt_src_dir" || exit
+
+    git clone https://code.qt.io/qt/qt5.git  .
+    git checkout v6.5.2
+    perl init-repository --module-subset=qtbase
+
+    ./configure -static -release \
+        -prefix "$qt_static_dir" \
+        -platform linux-clang \
+        -opensource -confirm-license \
+        -skip qtwebengine \
+        -no-qml \
+        -no-openssl \
+        -no-icu \
+        -nomake examples -nomake tests
+
+    make -j"$nproc"
+    make install
 }
 
 build_zclipboard() {
-    cd "$build_dir" || exit
-    
+     cd "$build_dir" || exit
+
+    export PATH="$qt_static_dir/bin:$PATH"
+
     cmake -G "Ninja" \
-    -DCMAKE_CXX_COMPILER=clang \
-    -DCMAKE_CXX_FLAGS="$opt_flags" \
-    $release_flags \
-    ..
+        -DCMAKE_CXX_COMPILER=clang++ \
+        -DCMAKE_CXX_FLAGS="$opt_flags" \
+        $release_flags \
+        -DCMAKE_PREFIX_PATH="$qt_static_dir" \
+        ..
 
     ninja -j "$nproc"
 }
 
 match_options() {
     case "$1" in 
-    "install-base")  install_base ;;
-
-    "mkdir-build")   create_build_dir ;;
+    "install-base") install_base ;;
+    "mkdir-build") create_build_dir ;;
+    "build-qt") build_qt_static ;;
     "release-build") build_zclipboard ;;
     *)
-        echo "Invalid args $1"
-        exit 1
-    ;;
+    echo "Usage: $0 {install-base |mkdir-build |build-qt |release-build}"
+    exit 1
 
     esac
 }
