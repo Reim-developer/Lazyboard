072c37f009bd965d1f329a20a68ed3da8c65a08b
Reim-developer <contact.kaxtr@gmail.com>
2025-05-14 14:51:10 +0700

    Add clipboard notification settings and handling for text and image changes

--- src/zUtils/zUtils.cc
+++ src/zUtils/zUtils.cc
@@ -1,6 +1,12 @@
 #include "include/zUtils.hpp"
+#include "include/settings.hpp"
 #include <QtGlobal>
 #include <QStandardPaths>
+#include <QSettings>
+#include <QClipboard>
+#include <QApplication>
+#include <QPointer>
+#include <QMimeData>
 
 using namespace zclipboard;
 
@@ -10,4 +16,37 @@ QString zUtils::getCachePath() {
 #else
     return QStandardPaths::writableLocation(QStandardPaths::CacheLocation);
 #endif
+}
+
+bool zUtils::getAutoHideSetting() {
+    QSettings settings;
+    return settings.value(AUTO_HIDE_SETTING, false).toBool();
+}
+
+bool zUtils::getAutoNotificationSetting() {
+    QSettings settings;
+    return settings.value(AUTO_NOTIFICATION_SETTING, false).toBool();
+}
+
+void zUtils::textClipboardChanges(QSystemTrayIcon *trayIcon, QClipboard *clipboard) {
+    connect(clipboard, &QClipboard::dataChanged, [trayIcon, clipboard]() {
+        const QMimeData *mimeData = clipboard->mimeData();
+
+        if (getAutoNotificationSetting() && mimeData->hasText()) {
+            trayIcon->showMessage("Clipboard updated",
+                                  "New text content has been copied to your clipboard.",
+                                  QSystemTrayIcon::Information, 5000);
+        }
+    });
+}
+
+void zUtils::imageClipboardChanges(QSystemTrayIcon *trayIcon, QClipboard *clipboard) {
+    connect(clipboard, &QClipboard::dataChanged, [trayIcon, clipboard]() {
+        const QMimeData *mimeData = clipboard->mimeData();
+        if (getAutoNotificationSetting() && mimeData->hasImage()) {
+            trayIcon->showMessage("Clipboard updated",
+                                  "New image has been copied to your clipboard.",
+                                  QSystemTrayIcon::Information, 5000);
+        }
+    });
 }
