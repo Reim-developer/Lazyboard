c44849922ce3902cf555df8c97055fb6eb820eba
Reim-developer <contact.kaxtr@gmail.com>
2025-05-14 14:51:13 +0700

    Add settings definitions for auto-hide and notification features

---
+++ src/zUtils/include/settings.hpp
@@ -0,0 +1,7 @@
+#ifndef SETTINGS_HPP
+#define SETTINGS_HPP
+#include <QStringLiteral>
+#define AUTO_HIDE_SETTING "settings/auto_hide"
+#define AUTO_NOTIFICATION_SETTING "settings/auto_notification"
+
+#endif  // SETTINGS_HPP
