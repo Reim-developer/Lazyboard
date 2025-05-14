7543f6defaba18c51a34ffd14c6561a110d53666
Reim-developer <contact.kaxtr@gmail.com>
2025-05-14 14:51:16 +0700

    Refactor zUtils class to inherit from QObject and add auto-hide and notification settings methods

--- src/zUtils/include/zUtils.hpp
+++ src/zUtils/include/zUtils.hpp
@@ -1,12 +1,21 @@
 #ifndef Z_UTILS_HPP
 #define Z_UTILS_HPP
 #include <QString>
+#include <QSystemTrayIcon>
+#include <QWidget>
+#include <QObject>
 
 namespace zclipboard {
-    class zUtils {
-        public:
-            static QString getCachePath();
-    };
-}
+class zUtils : public QObject {
+    Q_OBJECT
 
-#endif 
+   public:
+    static QString getCachePath();
+    static bool getAutoHideSetting();
+    static bool getAutoNotificationSetting();
+    static void textClipboardChanges(QSystemTrayIcon *trayIcon, QClipboard *clipboard);
+    static void imageClipboardChanges(QSystemTrayIcon *trayIcon, QClipboard *clipboard);
+};
+}  // namespace zclipboard
+
+#endif
