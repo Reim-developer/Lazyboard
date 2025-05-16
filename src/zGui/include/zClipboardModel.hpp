c07bbc5e42c566ab31486fc573451a6adb82082d
Reim-developer <contact.kaxtr@gmail.com>
2025-05-16 22:48:19 +0700

    Refactor zTableModel to update addTextItem and addImageItem methods to accept zClipboardItem for improved clarity and consistency

--- src/zGui/include/zClipboardModel.hpp
+++ src/zGui/include/zClipboardModel.hpp
@@ -33,14 +33,11 @@ class zTableModel : public QAbstractTableModel {
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
     QVariant headerData(int section, Qt::Orientation orientation,
                         int role = Qt::DisplayRole) const override;
-
     Qt::ItemFlags flags(const QModelIndex &index) const override;
     bool setData(const QModelIndex &index, const QVariant &value, int role) override;
 
-    void addTextItem(const QString &time, const QString &text, const QString &hash, int length,
-                     bool isPinned = false);
-    void addImageItem(const QString &time, const QString &hash, const QByteArray &imageData,
-                      bool isPinned = false);
+    void addTextItem(const zClipboardItem &item);
+    void addImageItem(const zClipboardItem &item);
     void clearData();
     void filterItems(const QString &searchText);
 
