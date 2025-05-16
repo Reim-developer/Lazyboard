934b4296cb17f9f130d955397d02bbcdee22db3c
Reim-developer <contact.kaxtr@gmail.com>
2025-05-17 00:02:44 +0700

    Add trans.sh script for language file translation automation

---
+++ scripts/trans.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+trans() {
+    local trans_file="language.hpp"
+    local trans_script="translate.py"
+    local gen_path="../src/language"
+
+    cd ".." || exit
+    cd "tools" || exit
+
+    python "$trans_script"
+    cp "$trans_file" "$gen_path"
+    rm -rf "$trans_file"
+}
+
+trans
