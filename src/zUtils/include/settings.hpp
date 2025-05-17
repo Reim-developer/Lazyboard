c5cccb31d597d9ac515acdefa3137b467d7b7779
Reim-developer <contact.kaxtr@gmail.com>
2025-05-17 18:16:01 +0700

    Refactor settings constants to use inline constexpr for better type safety

--- src/zUtils/include/settings.hpp
+++ src/zUtils/include/settings.hpp
@@ -1,7 +1,8 @@
 #ifndef SETTINGS_HPP
 #define SETTINGS_HPP
-#include <QStringLiteral>
-#define AUTO_HIDE_SETTING "settings/auto_hide"
-#define AUTO_NOTIFICATION_SETTING "settings/auto_notification"
+
+inline constexpr const char* AUTO_HIDE_SETTING = "settings/auto_hide";
+inline constexpr const char* AUTO_NOTIFICATION_SETTING = "settings/auto_notification";
+inline constexpr const char* LANGUAGE_SETTING = "settings/languages";
 
 #endif  // SETTINGS_HPP
