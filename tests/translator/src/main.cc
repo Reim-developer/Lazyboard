4d3172a96eab37fe3d050c5096efe61125749b1e
Reim-developer <contact.kaxtr@gmail.com>
2025-05-17 23:32:03 +0700

    Add main test file to output language disconnect messages

---
+++ tests/translator/src/main.cc
@@ -0,0 +1,12 @@
+/*
+* Auto generate Test base.
+*/
+#include "../../../src/language/include/language.hpp"
+#include <iostream>
+using namespace zclipboard::language;
+
+int main() {
+    std::cout << DISCONNECT_VI << std::endl;
+    std::cout << DISCONNECT_EN << std::endl;
+    return 0;
+}
