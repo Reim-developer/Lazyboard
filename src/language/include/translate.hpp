2f3045d5f5a736cda91776d4f8cb6fdf903ee81e
Reim-developer <contact.kaxtr@gmail.com>
2025-05-17 18:15:40 +0700

    Refactor Translate class to improve structure and readability

--- src/language/include/translate.hpp
+++ src/language/include/translate.hpp
@@ -6,9 +6,12 @@ namespace zclipboard::language {
 struct TranslateParams {
     QPushButton *button;
     const char *TRANS_VALUE;
+    const int languageType;
 };
 
 class Translate {
+   public:
+    enum LanguageType { ENGLISH, VIETNAMESE };
     static void buttonTrans(const TranslateParams &params);
 };
 }  // namespace zclipboard::language
