6f9f208824b0cb124947ecd61430eeedd14a1e39
Reim-developer <contact.kaxtr@gmail.com>
2025-05-17 18:15:50 +0700

    Add language settings to SettingButton for multilingual support

--- src/zGui/settingButton.cc
+++ src/zGui/settingButton.cc
@@ -1,9 +1,11 @@
 #include "include/settingButton.hpp"
+#include "../language/include/translate.hpp"
 #include "../zUtils/include/settings.hpp"
 #include <QStringLiteral>
 #include <QSettings>
 #include <QIcon>
 
+using zclipboard::language::Translate;
 using zclipboard::zGui::SettingButton;
 
 void SettingButton::addSettingButton(QWidget *window, QGridLayout *layout) {
@@ -22,7 +24,7 @@ void SettingButton::showSettingDialog(QWidget *parent) {
     layout = new QGridLayout(dialog);
 
     QIcon icon = QIcon(QStringLiteral(":/assets/assets/icon.png"));
-    constexpr int FIXED_WIDTH = 400;
+    constexpr int FIXED_WIDTH = 600;
     constexpr int FIXED_HEIGHT = 400;
 
     dialog->setWindowTitle(QStringLiteral("ZClipboard - Setting"));
@@ -37,6 +39,8 @@ void SettingButton::addGui(QGridLayout *layout) {
     QSettings *settings = new QSettings();
     autoHideCheckBox = new QCheckBox();
     autoNotificatonCheckBox = new QCheckBox();
+    QComboBox *languageBox = new QComboBox();
+    QLabel *languageDescription = new QLabel();
 
     bool isHide = settings->value(AUTO_HIDE_SETTING).toBool();
     bool isNotification = settings->value(AUTO_NOTIFICATION_SETTING).toBool();
@@ -49,19 +53,51 @@ void SettingButton::addGui(QGridLayout *layout) {
 
     addHideSetting({.checkbox = autoHideCheckBox, .settings = settings});
     addNotificationSetting({.checkbox = autoNotificatonCheckBox, .settings = settings});
+    addLanguageSetting({.settings = settings,
+                        .languageDescription = languageDescription,
+                        .comboBox = languageBox});
 
     layout->addWidget(autoHideCheckBox, 0, 0);
     layout->addWidget(autoNotificatonCheckBox, 1, 0);
+    layout->addWidget(languageDescription, 2, 1);
+    layout->addWidget(languageBox, 2, 0);
 }
 
 void SettingButton::addHideSetting(const SettingWidget &settingWidget) {
-    connect(settingWidget.checkbox, &QCheckBox::toggled, [this, settingWidget](bool isChecked) {
-        settingWidget.settings->setValue(AUTO_HIDE_SETTING, isChecked);
-    });
+    connect(settingWidget.checkbox, &QCheckBox::toggled,
+            [this, settings = settingWidget.settings](bool isChecked) {
+                settings->setValue(AUTO_HIDE_SETTING, isChecked);
+            });
 }
 
 void SettingButton::addNotificationSetting(const SettingWidget &settingWidget) {
-    connect(settingWidget.checkbox, &QCheckBox::toggled, [this, settingWidget](bool isChecked) {
-        settingWidget.settings->setValue(AUTO_NOTIFICATION_SETTING, isChecked);
-    });
+    connect(settingWidget.checkbox, &QCheckBox::toggled,
+            [this, settings = settingWidget.settings](bool isChecked) {
+                settings->setValue(AUTO_NOTIFICATION_SETTING, isChecked);
+            });
+}
+
+void SettingButton::addLanguageSetting(const SettingWidget &settingWidget) {
+    settingWidget.languageDescription->setText(QStringLiteral("Language Display"));
+    settingWidget.comboBox->addItem(QStringLiteral("English"),
+                                    static_cast<int>(Translate::ENGLISH));
+    settingWidget.comboBox->addItem(QStringLiteral("VietNamese"),
+                                    static_cast<int>(Translate::VIETNAMESE));
+
+    int currentLanguage =
+        settingWidget.settings->value(LANGUAGE_SETTING, static_cast<int>(Translate::ENGLISH))
+            .toInt();
+    for (int index = 0; index < settingWidget.comboBox->count(); ++index) {
+        if (settingWidget.comboBox->itemData(index).toInt() == currentLanguage) {
+            settingWidget.comboBox->setCurrentIndex(index);
+            break;
+        }
+    }
+
+    connect(settingWidget.comboBox, &QComboBox::currentIndexChanged, this,
+            [settings = settingWidget.settings, comboBox = settingWidget.comboBox](int index) {
+                int selectedLanguage = comboBox->itemData(index).toInt();
+                settings->setValue(LANGUAGE_SETTING, selectedLanguage);
+                settings->sync();
+            });
 }
