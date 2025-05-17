a80209f584161e7129a7d56c21a8ed103999241f
Reim-developer <contact.kaxtr@gmail.com>
2025-05-17 18:15:56 +0700

    Add language setting support to SettingWidget structure

--- src/zGui/include/settingButton.hpp
+++ src/zGui/include/settingButton.hpp
@@ -8,12 +8,17 @@
 #include <QCheckBox>
 #include <QGridLayout>
 #include <QSettings>
+#include <QComboBox>
+#include <QLabel>
 
 namespace zclipboard::zGui {
 
 struct SettingWidget {
     QCheckBox *checkbox;
     QSettings *settings;
+    QLabel *languageDescription;
+    QGridLayout *layout;
+    QComboBox *comboBox;
 };
 
 class SettingButton : public QObject {
@@ -27,6 +32,7 @@ class SettingButton : public QObject {
     void addGui(QGridLayout *layout);
     void addHideSetting(const SettingWidget &settingWidget);
     void addNotificationSetting(const SettingWidget &settingWidget);
+    void addLanguageSetting(const SettingWidget &settingWidget);
 
    private:
     QPushButton *settingButton;
