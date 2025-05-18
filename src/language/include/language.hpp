c428a627f191e7b209c10dc99e9793083f03e9ea
Reim-developer <contact.kaxtr@gmail.com>
2025-05-18 13:08:53 +0700

    Refactor language header to improve consistency and remove unnecessary namespace

--- src/language/include/language.hpp
+++ src/language/include/language.hpp
@@ -1,12 +1,8 @@
-#ifndef TRANSLATE_HPP
-#define TRANSLATE_HPP
-/*
-Generate by zClipboard Translate Tools.
-*/
+#ifndef TRANSLATOR_HPP 
+#define TRANSLATOR_HPP 
+// Generate by zClipboard Toolchain.
 
-namespace zclipboard::language {
-inline constexpr const char* DISCONNECT_VI = "\x4e\x67\xe1\xba\xaf\x74\x20\x6b\xe1\xba\xbf\x74\x20\x6e\xe1\xbb\x91\x69";
-inline constexpr const char* DISCONNECT_EN = "\x44\x69\x73\x63\x6f\x6e\x6e\x65\x63\x74";
-}
+inline constexpr const char DISCONNECT_VI[] = "\x4e\x67\xe1\xba\xaf\x74\x20\x6b\xe1\xba\xbf\x74\x20\x6e\xe1\xbb\x91\x69";
+inline constexpr const char DISCONNECT_EN[] = "\x44\x69\x73\x63\x6f\x6e\x6e\x65\x63\x74";
 
 #endif
