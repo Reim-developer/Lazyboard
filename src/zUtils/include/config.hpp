6c027e53c49b2bce17e87c4e7be6613dd3c053fb
Reim-developer <contact.kaxtr@gmail.com>
2025-05-18 17:31:06 +0700

    Update config.hpp to include expanded descriptions for APP_NAME, AUTHOR_NAME, and ICON_PATH

--- src/zUtils/include/config.hpp
+++ src/zUtils/include/config.hpp
@@ -1,8 +1,13 @@
 #ifndef CONFIG_HPP 
 #define CONFIG_HPP 
 // Generate by zClipboard Toolchain.
+// For more information. Please see tools/xml.
 
+// Expands to: zClipboard.
 inline constexpr const char APP_NAME[] = "\x7a\x43\x6c\x69\x70\x62\x6f\x61\x72\x64";
+// Expands to: ReimEmp.
 inline constexpr const char AUTHOR_NAME[] = "\x52\x65\x69\x6d\x45\x6d\x70";
+// Expands to: :/assets/assets/icon.png.
+inline constexpr const char ICON_PATH[] = "\x3a\x2f\x61\x73\x73\x65\x74\x73\x2f\x61\x73\x73\x65\x74\x73\x2f\x69\x63\x6f\x6e\x2e\x70\x6e\x67";
 
 #endif
