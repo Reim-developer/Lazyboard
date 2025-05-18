ec9017256878fd1204c28fb2b49c2c0252ec7ea7
Reim-developer <contact.kaxtr@gmail.com>
2025-05-18 13:09:05 +0700

    Add config header with APP_NAME and AUTHOR_NAME constants

---
+++ src/zUtils/include/config.hpp
@@ -0,0 +1,8 @@
+#ifndef CONFIG_HPP 
+#define CONFIG_HPP 
+// Generate by zClipboard Toolchain.
+
+inline constexpr const char APP_NAME[] = "\x7a\x43\x6c\x69\x70\x62\x6f\x61\x72\x64";
+inline constexpr const char AUTHOR_NAME[] = "\x52\x65\x69\x6d\x45\x6d\x70";
+
+#endif
