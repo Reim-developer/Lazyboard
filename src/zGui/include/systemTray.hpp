a7a25f886ed1739337b210a70b2eafe4d30811b4
Reim-developer <contact.kaxtr@gmail.com>
2025-05-18 20:29:08 +0700

    Add SystemTray class and SystemTrayWidget struct for system tray functionality

---
+++ src/zGui/include/systemTray.hpp
@@ -0,0 +1,33 @@
+#ifndef SYSTEM_TRAY_HPP
+#define SYSTEM_TRAY_HPP
+#include <QSystemTrayIcon>
+#include <QWidget>
+#include <QObject>
+#include <QMainWindow>
+
+namespace zclipboard::zGui {
+struct SystemTrayWidget {
+    QMainWindow *window;
+    QIcon icon;
+};
+class SystemTray : public QMainWindow {
+    Q_OBJECT
+   public:
+    void addSystemTray(const SystemTrayWidget &widget);
+
+   private:
+    void translatorDectect(QMainWindow *window);
+    void loadVietNameseTranslator(QMainWindow *window);
+    void loadEnglishTranslator(QMainWindow *window);
+    QSystemTrayIcon *trayIcon;
+    QMenu *trayMenu;
+    QAction *showGui;
+    QAction *hideGui;
+    QAction *quitGui;
+
+   private:
+    void addTrayMenuActions(QSystemTrayIcon *systemTrayIcon);
+};
+}  // namespace zclipboard::zGui
+
+#endif
