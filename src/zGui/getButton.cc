a64aa9a6d0852efaf941043fbfbbdbcadea7c636
Reim-developer <contact.kaxtr@gmail.com>
2025-05-20 01:25:28 +0700

    Refactor GetButton to use localized messages for button text and server status notifications

--- src/zGui/getButton.cc
+++ src/zGui/getButton.cc
@@ -1,18 +1,26 @@
 #include "include/getButton.hpp"
+#include "../zUtils/include/settings.hpp"
+#include "../language/include/language.hpp"
+#include "../zUtils/include/config.hpp"
 #include <QtNetwork/QTcpServer>
 #include <QtNetwork/QTcpSocket>
 #include <QApplication>
 #include <QClipboard>
 #include <QMessageBox>
+#include <QSettings>
 
+using zclipboard::language::TransValue;
 using zclipboard::zGui::GetButton;
 
 void GetButton::addGetButton(QWidget *window, QGridLayout *layout) {
     getButton = new QPushButton(window);
-    getButton->setText("Get clipboard from another device");
+    settings = new QSettings(AUTHOR_NAME, APP_NAME);
 
+    type = settings->value(LANGUAGE_SETTING).toInt();
     connect(getButton, &QPushButton::clicked, this, [this, window]() {
-        getButton->setText("Waiting connection device..");
+        const auto TYPE_TRANS = type ? GET_CONTENT_WAIT_BUTTON_VI : GET_CONTENT_WAIT_BUTTON_EN;
+
+        getButton->setText(TYPE_TRANS);
         createReceiverServer(window);
         peer = new PeerDiscovery(45454, window);
     });
@@ -26,12 +34,19 @@ void GetButton::createReceiverServer(QWidget *parent) {
     }
 
     if (server && server->isListening()) {
-        QMessageBox::information(parent, "Status", "Server is already running.");
+        auto const DIALOG_TITLE = type ? DIALOG_INFO_TITLE_VI : DIALOG_INFO_TITLE_EN;
+        auto const DIALOG_MESSAGE = type ? SERVER_ALREADY_ERR_VI : SERVER_ALREADY_ERR_EN;
+
+        QMessageBox::information(parent, DIALOG_TITLE, DIALOG_MESSAGE);
         return;
     }
 
     if (!server->listen(QHostAddress::AnyIPv4, 8000)) {
-        QMessageBox::critical(parent, "Error", "Could not start server: " + server->errorString());
+        auto const DIALOG_TITLE = type ? DIALOG_ERROR_TITLE_VI : DIALOG_ERROR_TITLE_EN;
+        auto const DIALOG_MESSAGE =
+            type ? COULD_NOT_START_SERVER_ERR_VI : COULD_NOT_START_SERVER_ERR_EN;
+
+        QMessageBox::critical(parent, DIALOG_TITLE, DIALOG_MESSAGE + server->errorString());
         server->deleteLater();
         server = nullptr;
         return;
