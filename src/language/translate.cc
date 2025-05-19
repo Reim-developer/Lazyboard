34e5371f0ea0fe6135f3c020bf6b86a6708003bf
Reim-developer <contact.kaxtr@gmail.com>
2025-05-19 18:13:17 +0700

    Refactor translatorWidget methods to use TransValue struct for language text handling

--- src/language/translate.cc
+++ src/language/translate.cc
@@ -2,26 +2,60 @@
 
 using zclipboard::language::Translate;
 
-void Translate::translatorWidget(QPushButton* button, LanguageType type, const char* LANGUAGE_EN,
-                                 const char* LANGUAGE_VI) {
+void Translate::translatorWidget(QPushButton* button, LanguageType type, const TransValue& trans) {
     switch (type) {
         case ENGLISH:
-            button->setText(LANGUAGE_EN);
+            button->setText(trans.LANGUAGE_EN);
             break;
 
         case VIETNAMESE:
-            button->setText(LANGUAGE_VI);
+            button->setText(trans.LANGUAGE_VI);
     }
 }
 
-void Translate::translatorWidget(QLineEdit* lineEdit, LanguageType type, const char* LANGUAGE_EN,
-                                 const char* LANGUAGE_VI) {
+void Translate::translatorWidget(QLineEdit* lineEdit, LanguageType type, const TransValue& trans) {
     switch (type) {
         case ENGLISH:
-            lineEdit->setPlaceholderText(LANGUAGE_EN);
+            lineEdit->setPlaceholderText(trans.LANGUAGE_EN);
             break;
 
         case VIETNAMESE:
-            lineEdit->setPlaceholderText(LANGUAGE_VI);
+            lineEdit->setPlaceholderText(trans.LANGUAGE_VI);
+    }
+}
+
+void Translate::translatorWidget(QCheckBox* checkBox, LanguageType type, const TransValue& trans) {
+    switch (type) {
+        case ENGLISH:
+            checkBox->setText(trans.LANGUAGE_EN);
+            break;
+
+        case VIETNAMESE:
+            checkBox->setText(trans.LANGUAGE_VI);
+            break;
+    }
+}
+
+void Translate::translatorWidget(QLabel* label, LanguageType type, const TransValue& trans) {
+    switch (type) {
+        case ENGLISH:
+            label->setText(trans.LANGUAGE_EN);
+            break;
+
+        case VIETNAMESE:
+            label->setText(trans.LANGUAGE_VI);
+            break;
+    }
+}
+
+void Translate::translatorWidget(QDialog* dialog, LanguageType type, const TransValue& trans) {
+    switch (type) {
+        case ENGLISH:
+            dialog->setWindowTitle(trans.LANGUAGE_EN);
+            break;
+
+        case VIETNAMESE:
+            dialog->setWindowTitle(trans.LANGUAGE_VI);
+            break;
     }
 }
