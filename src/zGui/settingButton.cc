562ff9de22aaaf0aa65ac31ad9437b437d7c0ccb
Reim-developer <contact.kaxtr@gmail.com>
2025-05-19 18:13:27 +0700

    Refactor SettingButton to utilize TransValue for improved language handling in dialog and settings

--- src/zGui/settingButton.cc
+++ src/zGui/settingButton.cc
@@ -2,11 +2,14 @@
 #include "../language/include/translate.hpp"
 #include "../zUtils/include/settings.hpp"
 #include "../zUtils/include/config.hpp"
+#include "../zUtils/include/zUtils.hpp"
+#include "../language/include/language.hpp"
 #include <QStringLiteral>
 #include <QSettings>
 #include <QIcon>
 
 using zclipboard::language::Translate;
+using zclipboard::language::TransValue;
 using zclipboard::zGui::SettingButton;
 
 void SettingButton::addSettingButton(QWidget *window, QGridLayout *layout) {
@@ -21,14 +24,21 @@ void SettingButton::addSettingButton(QWidget *window, QGridLayout *layout) {
 }
 
 void SettingButton::showSettingDialog(QWidget *parent) {
+    settings = new QSettings(AUTHOR_NAME, APP_NAME);
     QDialog *dialog = new QDialog(parent);
     layout = new QGridLayout(dialog);
 
-    QIcon icon = QIcon(QStringLiteral(":/assets/assets/icon.png"));
+    QIcon icon = QIcon(ICON_PATH);
     constexpr int FIXED_WIDTH = 600;
     constexpr int FIXED_HEIGHT = 400;
 
-    dialog->setWindowTitle(QStringLiteral("ZClipboard - Setting"));
+    TransValue dialogValue{.LANGUAGE_EN = SETTING_DIALOG_EN, .LANGUAGE_VI = SETTING_DIALOG_VI};
+
+    const int LANGUAGE = settings->value(LANGUAGE_SETTING).toInt();
+    const auto TYPE = zUtils::languageTypeCast(LANGUAGE);
+
+    Translate::translatorWidget(dialog, TYPE, dialogValue);
+
     dialog->setFixedSize(FIXED_WIDTH, FIXED_HEIGHT);
     dialog->setWindowIcon(icon);
     dialog->setLayout(layout);
@@ -37,20 +47,27 @@ void SettingButton::showSettingDialog(QWidget *parent) {
 }
 
 void SettingButton::addGui(QGridLayout *layout) {
-    QSettings *settings = new QSettings(AUTHOR_NAME, APP_NAME);
     autoHideCheckBox = new QCheckBox();
     autoNotificatonCheckBox = new QCheckBox();
     QComboBox *languageBox = new QComboBox();
     QLabel *languageDescription = new QLabel();
 
+    const int LANGUAGE = settings->value(LANGUAGE_SETTING).toInt();
+    const auto TYPE = zUtils::languageTypeCast(LANGUAGE);
+
     bool isHide = settings->value(AUTO_HIDE_SETTING).toBool();
     bool isNotification = settings->value(AUTO_NOTIFICATION_SETTING).toBool();
 
-    autoHideCheckBox->setText(QStringLiteral("Minimize to system tray on window close."));
-    autoNotificatonCheckBox->setText(QStringLiteral("Notify me when clipboard changes."));
-
     autoHideCheckBox->setChecked(isHide);
     autoNotificatonCheckBox->setChecked(isNotification);
+
+    TransValue minimizeValue{.LANGUAGE_EN = MINIMIZE_CHECKBOX_EN,
+                             .LANGUAGE_VI = MINIMIZE_CHECKBOX_VI};
+    TransValue notifiValue{.LANGUAGE_EN = NOTIFICATION_CHECKBOX_EN,
+                           .LANGUAGE_VI = NOTIFICATION_CHECKBOX_VI};
+
+    Translate::translatorWidget(autoHideCheckBox, TYPE, minimizeValue);
+    Translate::translatorWidget(autoNotificatonCheckBox, TYPE, notifiValue);
 
     addHideSetting({.checkbox = autoHideCheckBox, .settings = settings});
     addNotificationSetting({.checkbox = autoNotificatonCheckBox, .settings = settings});
@@ -79,7 +96,13 @@ void SettingButton::addNotificationSetting(const SettingWidget &settingWidget) {
 }
 
 void SettingButton::addLanguageSetting(const SettingWidget &settingWidget) {
-    settingWidget.languageDescription->setText(QStringLiteral("Language Display"));
+    const int LANGUAGE = settings->value(LANGUAGE_SETTING).toInt();
+    const auto TYPE = zUtils::languageTypeCast(LANGUAGE);
+
+    TransValue transValue{.LANGUAGE_EN = LABEL_DESCRIPTION_EN, .LANGUAGE_VI = LABEL_DESCRIPTION_VI};
+
+    Translate::translatorWidget(settingWidget.languageDescription, TYPE, transValue);
+
     settingWidget.comboBox->addItem(QStringLiteral("English"),
                                     static_cast<int>(Translate::ENGLISH));
     settingWidget.comboBox->addItem(QStringLiteral("VietNamese"),
