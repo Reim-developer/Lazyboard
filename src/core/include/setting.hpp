a90ab50086c4b902e3cf53b8cbb74a3bb0eebc3e
Reim-developer <contact.kaxtr@gmail.com>
2025-05-22 20:50:00 +0700

    Add SettingCore class and SettingCoreParams struct for settings management

---
+++ src/core/include/setting.hpp
@@ -0,0 +1,33 @@
+#ifndef CORE_SETTING_HPP
+#define CORE_SETTING_HPP
+#include <QComboBox>
+#include <QDialog>
+#include <QObject>
+#include <QSettings>
+#include <QLabel>
+#include <QCheckBox>
+
+namespace zclipboard::core {
+
+// clang-format off
+struct SettingCoreParams {
+    QDialog *dialog;
+    QSettings *settings;
+    QLabel *languageDescription;
+    QComboBox *comboBox;
+    QCheckBox *autoHideCheckBox;
+    QCheckBox *autoNotificatonCheckBox;
+    QCheckBox *checkBox;
+};
+// clang-format on
+
+class SettingCore : public QObject {
+    Q_OBJECT
+   public:
+    void addLanguageSetting(const SettingCoreParams &params);
+    void addHideSetting(const SettingCoreParams &params);
+    void addNotificationSetting(const SettingCoreParams &params);
+};
+}  // namespace zclipboard::core
+
+#endif  // CORE_SETTING_HPP
