ca6a54761cc863548b0056e23fea43bc8d4fdc2c
Reim-developer <contact.kaxtr@gmail.com>
2025-05-24 00:54:57 +0700

    Add constexpr const char to optimize performance

--- src/zSQL/include/zSQL.hpp
+++ src/zSQL/include/zSQL.hpp
@@ -21,7 +21,10 @@ class zManagerSQL {
                                              const QVariantMap &params = QVariantMap());
 
    private:
-    const QString Z_DB_NAME = "ZClipboardDB";
+    static const constexpr char Z_DB_NAME[] = "ZClipboardDB";
+    static const constexpr char SQL_TYPE[] = "QSQLITE";
+    static const constexpr char DATABASE_FILE[] = "zclipboard.db";
+
     QSqlDatabase zDB;
     void zInitCache();
 };
