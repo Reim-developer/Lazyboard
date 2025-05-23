49dd67e3e1f37bac356c893504db0bc32441f357
Reim-developer <contact.kaxtr@gmail.com>
2025-05-24 00:12:54 +0700

    Add language selection functionality to SettingCore

--- src/core/setting.cc
+++ src/core/setting.cc
@@ -1,9 +1,11 @@
 #include "include/setting.hpp"
 #include "../zGui/include/LanguageManager.hpp"
+#include "../language/include/translate.hpp"
 #include "../zUtils/include/settings.hpp"
 #include "../language/include/language.hpp"
 
 using zclipboard::core::SettingCore;
+using zclipboard::language::Translate;
 using zclipboard::zGui::LanguageManager;
 
 void SettingCore::addHideSetting(const SettingCoreParams &params) {
@@ -21,6 +23,24 @@ void SettingCore::addNotificationSetting(const SettingCoreParams &params) {
 }
 
 void SettingCore::addLanguageSetting(const SettingCoreParams &params) {
+    const int TYPE = params.settings->value(LANGUAGE_SETTING).toInt();
+    const auto LABEL_TITLE = TYPE ? LABEL_DESCRIPTION_VI : LABEL_DESCRIPTION_EN;
+
+    params.languageDescription->setText(LABEL_TITLE);
+
+    params.comboBox->addItem(QStringLiteral("English"), static_cast<int>(Translate::ENGLISH));
+    params.comboBox->addItem(QStringLiteral("VietNamese"), static_cast<int>(Translate::VIETNAMESE));
+
+    int currentLanguage =
+        params.settings->value(LANGUAGE_SETTING, static_cast<int>(Translate::ENGLISH)).toInt();
+
+    for (int index = 0; index < params.comboBox->count(); ++index) {
+        if (params.comboBox->itemData(index).toInt() == currentLanguage) {
+            params.comboBox->setCurrentIndex(index);
+            break;
+        }
+    }
+
     connect(params.comboBox, &QComboBox::currentIndexChanged, this, [params](int index) {
         int selectedLanguage = params.comboBox->itemData(index).toInt();
         params.settings->setValue(LANGUAGE_SETTING, selectedLanguage);
