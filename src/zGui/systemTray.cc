693087b02f67215ec4473a276c58d62a3612602e
Reim-developer <contact.kaxtr@gmail.com>
2025-05-24 00:13:10 +0700

    Refactor SystemTray to improve memory management and streamline parameter handling

--- src/zGui/systemTray.cc
+++ src/zGui/systemTray.cc
@@ -1,9 +1,7 @@
 #include "include/systemTray.hpp"
 #include "../language/include/language.hpp"
-#include "../zUtils/include/zUtils.hpp"
 #include "../zUtils/include/settings.hpp"
 #include "../zUtils/include/config.hpp"
-#include "../language/include/translate.hpp"
 #include <QSettings>
 #include <QMenu>
 #include "include/zwindow.hpp"
@@ -14,7 +12,6 @@
 
 using zclipboard::core::SystemTrayCore;
 using zclipboard::core::SystemTrayParams;
-using zclipboard::language::Translate;
 using zclipboard::zGui::SystemTray;
 using zclipboard::zGui::SystemTrayWidget;
 
@@ -24,44 +21,26 @@ void SystemTray::addSystemTray(const SystemTrayWidget &widget) {
     trayIcon = new QSystemTrayIcon(widget.window);
     trayMenu = new QMenu();
     systemTrayCore = new SystemTrayCore();
-    QSettings setting(AUTHOR_NAME, APP_NAME);
+    QSettings *setting = new QSettings(AUTHOR_NAME, APP_NAME);
 
-    const auto TYPE = setting.value(LANGUAGE_SETTING).toInt();
+    const auto TYPE = setting->value(LANGUAGE_SETTING).toInt();
 
     // clang-format off
     struct SystemTrayParams systemTrayParams {
-            .trayMenu = trayMenu,
             .window = window,
+            .settings = setting,
+            .trayMenu = trayMenu,
             .TYPE = TYPE
     };
     // clang-format on
 
+    systemTrayCore->translatorDectect(systemTrayParams);
     systemTrayCore->updateSwitchLanguageInstance(systemTrayParams);
-    translatorDectect(widget.window);
 
     trayIcon->setIcon(widget.icon);
     trayIcon->setToolTip(TOOL_TIP_TEXT_EN);
     trayIcon->setContextMenu(trayMenu);
     trayIcon->show();
-}
-
-void SystemTray::translatorDectect(QMainWindow *window) {
-    QSettings *settings = new QSettings(AUTHOR_NAME, APP_NAME);
-    if (!zUtils::getLanguageSetting()) {
-        settings->setValue(LANGUAGE_SETTING, Translate::ENGLISH);
-    }
-
-    const int TYPE = settings->value(LANGUAGE_SETTING).toInt();
-
-    // clang-format off
-    struct SystemTrayParams systemTrayParams {
-        .trayMenu = trayMenu,
-        .window = window,
-        .TYPE = TYPE
-    };
-    // clang-format on
-
-    systemTrayCore->loadTranslator(systemTrayParams);
 }
 
 QSystemTrayIcon *SystemTray::getSystemTrayIcon() {
