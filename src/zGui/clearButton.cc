167b35e4f2671ebce96ffd4db37b90bc4d936844
Reim-developer <contact.kaxtr@gmail.com>
2025-05-23 22:34:32 +0700

    Refactor ClearButton to use ClearCore for cache clearing functionality

--- src/zGui/clearButton.cc
+++ src/zGui/clearButton.cc
@@ -6,36 +6,28 @@
 #include "include/ztable.hpp"
 #include "../zUtils/include/config.hpp"
 #include <QSettings>
-#include "../zUtils/include/settings.hpp"
-#include "../language/include/language.hpp"
+#include "../core/include/clear.hpp"
 
+using zclipboard::core::ClearCore;
+using zclipboard::core::ClearCoreParams;
 using zclipboard::zGui::ClearButton;
 
 void ClearButton::addClearButton(QGridLayout *layout, ZTable *table) {
     clearButton = new QPushButton();
     settings = new QSettings(AUTHOR_NAME, APP_NAME);
+    ClearCore *clearCore = new ClearCore();
 
     layout->addWidget(clearButton, 0, 2);
-    connect(clearButton, &QPushButton::clicked,
-            [this, table]() { clearCache(table, clearButton); });
-}
-
-void ClearButton::clearCache(ZTable *table, QPushButton *clearButton) {
-    QString query = R"(
-        DROP TABLE IF EXISTS clipboard
-    )";
-    table->getZSQL().executeQuery(query, QVariantMap());
 
     // clang-format off
-    const int LANGUAGE_TYPE = settings->value(LANGUAGE_SETTING).toInt();
-    const auto BUTTON_TEXT = LANGUAGE_TYPE ? CLEAR_HISTORY_VI : CLEAR_HISTORY_EN;
-    const auto ACTION_BUTTON_TEXT = LANGUAGE_TYPE ? CLEAR_HISTORY_ACTION_VI : CLEAR_HISTORY_ACTION_EN;
-    // clang-format on
-
-    clearButton->setText(ACTION_BUTTON_TEXT);
-    table->getZModel()->clearData();
-
-    QTimer::singleShot(1500, [clearButton, BUTTON_TEXT]() { clearButton->setText(BUTTON_TEXT); });
+    connect(clearButton, &QPushButton::clicked, [this, table, clearCore]() {  
+        struct ClearCoreParams params {
+            .table = table,
+            .button = clearButton,
+            .settings = settings,
+        };
+        clearCore->clearCache(params);
+    });
 }
 
 QPushButton *ClearButton::getClearButton() {
