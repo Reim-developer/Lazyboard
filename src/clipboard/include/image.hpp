857a257753f80175a532482fe8ca00d0010d2eaf
Reim-developer <contact.kaxtr@gmail.com>
2025-05-24 21:38:55 +0700

    Refactor addClipboardImage to use const reference for zManagerSQL parameter

--- src/clipboard/include/image.hpp
+++ src/clipboard/include/image.hpp
@@ -8,16 +8,15 @@
 #include "../../zSQL/include/zSQL.hpp"
 #include "../../zGui/include/zClipboardModel.hpp"
 
-using zclipboard::zSQL::zManagerSQL;
 using zclipboard::zGui::zTableModel;
+using zclipboard::zSQL::zManagerSQL;
 
 namespace zclipboard::clipboard {
-    class zImage {
-        public:
-            static void addClipboardImage(zTableModel *zModelTable, QClipboard *zClipboard, 
-                                          zManagerSQL zSQL, QSet<QString> &zExistingImages);
+class zImage {
+   public:
+    static void addClipboardImage(zTableModel *zModelTable, QClipboard *zClipboard,
+                                  const zManagerSQL &zSQL, QSet<QString> &zExistingImages);
+};
+}  // namespace zclipboard::clipboard
 
-    };
-} // namespace zclipboard::clipboard
-
-#endif // IMAGE_HPP
+#endif  // IMAGE_HPP
