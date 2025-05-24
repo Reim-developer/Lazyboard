f33403721c79225e5d09f683e275e1da90526325
Reim-developer <contact.kaxtr@gmail.com>
2025-05-24 21:38:52 +0700

    Refactor addClipboardHistoryFromDB to use const reference for zManagerSQL parameter

--- src/clipboard/include/cache.hpp
+++ src/clipboard/include/cache.hpp
@@ -4,14 +4,14 @@
 #include "../../zSQL/include/zSQL.hpp"
 #include "../../zGui/include/zClipboardModel.hpp"
 
-using zclipboard::zSQL::zManagerSQL;
 using zclipboard::zGui::zTableModel;
+using zclipboard::zSQL::zManagerSQL;
 
 namespace zclipboard::clipboard {
-    class zCacheManager {
-        public:
-            void addClipboardHistoryFromDB(zTableModel *zModelTable, zManagerSQL zSQL);
-    };
-} // namespace zclipboard::clipboard
+class zCacheManager {
+   public:
+    void addClipboardHistoryFromDB(zTableModel *zModelTable, const zManagerSQL &zSQL);
+};
+}  // namespace zclipboard::clipboard
 
-#endif // CACHE_HPP
+#endif  // CACHE_HPP
