eec4fabb2840fe7bc913434987072863d95ffd23
Reim-developer <contact.kaxtr@gmail.com>
2025-05-25 19:24:50 +0700

    Refactor clearCache to remove clipboard table and initialize database connection

--- src/core/clear.cc
+++ src/core/clear.cc
@@ -1,15 +1,27 @@
 #include "include/clear.hpp"
 #include <QTimer>
+#include <QFile>
+#include <QtSql/QSqlDatabase>
 #include "../zUtils/include/settings.hpp"
 #include "../language/include/language.hpp"
+#include "../zUtils/include/zUtils.hpp"
 
 using zclipboard::core::ClearCore;
 
 void ClearCore::clearCache(const ClearCoreParams &params) {
-    QString query = R"(
-        DROP TABLE IF EXISTS clipboard
-    )";
-    params.table->getZSQL().executeQuery(query, QVariantMap());
+    const constexpr char DB_FILE[] = "zclipboard.db";
+    const constexpr char DB_NAME[] = "ZClipboardDB";
+    const auto cachePath = zUtils::getCachePath() + "/" + DB_FILE;
+
+    QSqlDatabase database = QSqlDatabase::database(DB_NAME);
+    if (database.isOpen()) {
+        database.close();
+    }
+
+    if (QFile(cachePath).exists()) {
+        QFile(cachePath).remove();
+    }
+    params.table->getZSQL().setupinitDB();
 
     // clang-format off
     const int LANGUAGE_TYPE = params.settings->value(LANGUAGE_SETTING).toInt();
