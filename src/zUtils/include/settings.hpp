87870e1d4f39c8529a26c0292109df3f50fb96b1
Reim-developer <contact.kaxtr@gmail.com>
2025-05-26 21:30:20 +0700

    Add new constexpr char for implement 'Them Setting'

--- src/zUtils/include/settings.hpp
+++ src/zUtils/include/settings.hpp
@@ -4,5 +4,6 @@
 inline constexpr const char* AUTO_HIDE_SETTING = "settings/auto_hide";
 inline constexpr const char* AUTO_NOTIFICATION_SETTING = "settings/auto_notification";
 inline constexpr const char* LANGUAGE_SETTING = "settings/languages";
+inline constexpr const char* THEME_SETTING = "settings/theme";
 
 #endif  // SETTINGS_HPP
