0c8477bd70f0795c5e977214b283d063a2b85032
Reim-developer <contact.kaxtr@gmail.com>
2025-05-26 21:28:11 +0700

    Add Application class, and add public void function loadTheme, for implement theme preload.

---
+++ src/preload/include/theme.hpp
@@ -0,0 +1,11 @@
+#ifndef ZCLIPBOARD_MAIN_HPP
+#define ZCLIPBOARD_MAIN_HPP
+
+namespace zclipboard::preload {
+class Application {
+   public:
+    void loadTheme();
+};
+}  // namespace zclipboard::preload
+
+#endif  // ZCLIPBOARD_MAIN_HPP
