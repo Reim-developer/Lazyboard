ceb7e11513377a8a56c4755961cb6363e8b97790
Reim-developer <contact.kaxtr@gmail.com>
2025-05-26 21:25:45 +0700

    Modify zClipboard.cc to implement theme preload

--- src/zclipboard.cc
+++ src/zclipboard.cc
@@ -1,13 +1,20 @@
 #include "zGui/include/zwindow.hpp"
+#include "preload/include/theme.hpp"
 #include <QApplication>
+#include <memory>
 
+using std::make_unique;
+using std::unique_ptr;
+using zclipboard::preload::Application;
 using zclipboard::zGui::ZWindow;
 
 int main(int argc, char *argv[]) {
+    unique_ptr<Application> zClipboard = make_unique<Application>();
+    zClipboard->loadTheme();
+
     QApplication application(argc, argv);
-    
     ZWindow zWindow;
     zWindow.show();
-    
-    return  application.exec();
+
+    return application.exec();
 }
