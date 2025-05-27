df1c14a1343299c1aa94023318cfba6a55672371
Reim-developer <contact.kaxtr@gmail.com>
2025-05-27 20:48:38 +0700

    Refactor theme management: add dark and light theme methods, update theme color constants

--- src/preload/include/theme.hpp
+++ src/preload/include/theme.hpp
@@ -1,18 +1,37 @@
 #ifndef ZCLIPBOARD_MAIN_HPP
 #define ZCLIPBOARD_MAIN_HPP
+#include <QApplication>
+#include "../../core/include/enum.hpp"
+
+using zclipboard::core::Theme;
 
 namespace zclipboard::preload {
 class Application {
    private:
     void setDefaultTheme();
+    void setDarkTheme(QApplication &application);
+
+    void setLightTheme(QApplication &application);
 
    public:
-    void loadTheme();
+    bool loadDefaultTheme();
+    void loadSettingTheme(QApplication &application);
 
    private:
-    static const constexpr char LINUX_DEFAULT_THEME[] = "Fusion";
-    static const constexpr char WINDOWS_DEFAULT_THEME[] = "Windows";
-    static const constexpr char MACOS_DEFAULT_THEME[] = "macOS";
+    // clang-format off
+    static inline const QColor DARK_BG_COLOR {47, 49, 54};
+    static inline const QColor DARK_FG_COLOR {255, 255, 255};
+    
+    static inline const QColor LIGHT_BG_COLOR {255, 255, 255};
+    static inline const QColor LIGHT_FG_COLOR {0, 0, 0};
+    // clang-format on
+
+   private:
+    static inline const constexpr int THEME_DARK = static_cast<int>(Theme::DARK);
+    static inline const constexpr int THEME_LIGHT = static_cast<int>(Theme::LIGHT);
+    static inline const constexpr char LINUX_DEFAULT_THEME[] = "Fusion";
+    static inline const constexpr char WINDOWS_DEFAULT_THEME[] = "Windows";
+    static inline const constexpr char MACOS_DEFAULT_THEME[] = "macOS";
 };
 }  // namespace zclipboard::preload
 
