d7e77fbc4710b86db9ac41a8e0a97396ff0afb29
Reim-developer <contact.kaxtr@gmail.com>
2025-05-27 14:45:03 +0700

    Refactor theme loading logic to use setDefaultTheme function

--- src/preload/theme.cc
+++ src/preload/theme.cc
@@ -1,10 +1,32 @@
 #include "include/theme.hpp"
+#include <QApplication>
+#include <QStyleFactory>
 #include "../zUtils/include/zUtils.hpp"
+#include "../zUtils/include/settings.hpp"
 
 using zclipboard::preload::Application;
 
+void Application::setDefaultTheme() {
+    switch (zUtils::hasPlatform()) {
+        case Platform::LINUX:
+            QApplication::setStyle(QStyleFactory::create(LINUX_DEFAULT_THEME));
+            break;
+
+        case Platform::MACOS:
+            QApplication::setStyle(QStyleFactory::create(MACOS_DEFAULT_THEME));
+            break;
+
+        case Platform::WINDOWS:
+            QApplication::setStyle(QStyleFactory::create(WINDOWS_DEFAULT_THEME));
+            break;
+
+        case Platform::UNKNOWN:
+            break;
+    }
+}
+
 void Application::loadTheme() {
-    if (!zUtils::hasDefaultSystemTheme()) {
-        zUtils::setDefaultTheme();
+    if (!zUtils::hasSetting(THEME_SETTING)) {
+        setDefaultTheme();
     }
 }
