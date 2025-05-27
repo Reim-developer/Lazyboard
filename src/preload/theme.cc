482fe6bc0639013ecd84569d8e561270cb9636a2
Reim-developer <contact.kaxtr@gmail.com>
2025-05-27 20:48:34 +0700

    Implement theme management: add light and dark theme settings, update default theme logic

--- src/preload/theme.cc
+++ src/preload/theme.cc
@@ -1,9 +1,15 @@
 #include "include/theme.hpp"
 #include <QApplication>
 #include <QStyleFactory>
+#include <QSettings>
+#include <QPalette>
+#include <QColor>
 #include "../zUtils/include/zUtils.hpp"
 #include "../zUtils/include/settings.hpp"
+#include "../zUtils/include/config.hpp"
+#include "../core/include/enum.hpp"
 
+using zclipboard::core::Theme;
 using zclipboard::preload::Application;
 
 void Application::setDefaultTheme() {
@@ -25,8 +31,69 @@ void Application::setDefaultTheme() {
     }
 }
 
-void Application::loadTheme() {
+void Application::setLightTheme(QApplication &application) {
+    QPalette palette;
+
+    /*
+    * Background color.
+    */
+    palette.setColor(QPalette::Button, LIGHT_BG_COLOR);
+    palette.setColor(QPalette::Window, LIGHT_BG_COLOR);
+    palette.setColor(QPalette::Base, LIGHT_BG_COLOR);
+
+    /*
+    * Foreground color.
+    */
+    palette.setColor(QPalette::ButtonText, LIGHT_FG_COLOR);
+    palette.setColor(QPalette::Text, LIGHT_FG_COLOR);
+
+    application.setPalette(palette);
+}
+
+void Application::setDarkTheme(QApplication &application) {
+    QPalette palette;
+
+    /*
+    * Background color.
+    */
+    palette.setColor(QPalette::Button, DARK_BG_COLOR);
+    palette.setColor(QPalette::Window, DARK_BG_COLOR);
+    palette.setColor(QPalette::Base, DARK_BG_COLOR);
+
+    /*
+    * Foreground color.
+    */
+    palette.setColor(QPalette::ButtonText, DARK_FG_COLOR);
+    palette.setColor(QPalette::Text, DARK_FG_COLOR);
+
+    application.setPalette(palette);
+}
+
+void Application::loadSettingTheme(QApplication &application) {
+    QSettings settings(AUTHOR_NAME, APP_NAME);
+    const auto SETTING_THEME = settings.value(THEME_SETTING).toInt();
+
+    switch (SETTING_THEME) {
+        case THEME_DARK:
+            setDarkTheme(application);
+            break;
+
+        case THEME_LIGHT:
+            setLightTheme(application);
+            break;
+    }
+}
+
+bool Application::loadDefaultTheme() {
     if (!zUtils::hasSetting(THEME_SETTING)) {
         setDefaultTheme();
+        const auto DEFAULT_THEME = static_cast<int>(Theme::SYSTEM);
+
+        QSettings settings(AUTHOR_NAME, APP_NAME);
+        settings.setValue(THEME_SETTING, DEFAULT_THEME);
+
+        return true;
     }
+
+    return false;
 }
