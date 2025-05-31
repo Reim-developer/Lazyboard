bfd932fcca2c4e87d4c77e4dba1daa5dc44c9768
Reim-developer <contact.kaxtr@gmail.com>
2025-05-31 19:36:13 +0700

    Implement password input dialog with language-specific tooltips and button texts

---
+++ src/zGui/passwordDialog.cc
@@ -0,0 +1,92 @@
+#include "include/dialog.hpp"
+#include "../language/include/language.hpp"
+#include "../zUtils/include/config.hpp"
+#include "../zUtils/include/settings.hpp"
+#include "../listener/include/dialogListener.hpp"
+#include <QSettings>
+#include <QDialog>
+#include <QLineEdit>
+#include <QGridLayout>
+#include <QSettings>
+
+// clang-format off
+#if defined(Z_DEBUG)
+    #include <QDebug>
+#endif
+// clang-format on
+
+using zclipboard::listener::changePasswordParameters;
+using zclipboard::listener::ListenerDialog;
+using zclipboard::zGui::GuiDialog;
+
+void GuiDialog::showInputPassword(QDialog *parent) {
+    QDialog *dialog = new QDialog(parent);
+    QSettings settings(AUTHOR_NAME, APP_NAME);
+
+    const int LANGUAGUE_TYPE = settings.value(LANGUAGE_SETTING).toInt();
+    const auto DIALOG_TITLE = LANGUAGUE_TYPE ? INPUT_PASSWORD_DIALOG_VI : INPUT_PASSWORD_DIALOG_EN;
+
+    // clang-format off
+    /*
+    * Debug flag only.
+    * Use with CMake: -DZ_DEBUG=1
+    */
+    #if defined(Z_DEBUG)
+        qDebug() << "Current language setting: " << LANGUAGUE_TYPE;
+        qDebug() << "Current dialog title: " << DIALOG_TITLE;
+    #endif
+
+    const auto function = [parent]() {
+        parent->show();
+    };
+    // clang-format on
+
+    connect(dialog, &QDialog::finished, this, function);
+
+    setupGui(dialog);
+
+    dialog->setWindowTitle(DIALOG_TITLE);
+    dialog->resize(DIALOG_WIDTH, DIALOG_HEIGHT);
+    dialog->exec();
+}
+
+void GuiDialog::setupGui(QDialog *parent) {
+    QGridLayout *layout = new QGridLayout(parent);
+    QSettings *settings = new QSettings(AUTHOR_NAME, APP_NAME, parent);
+    ListenerDialog *listenerDialog = new ListenerDialog();
+
+    passwordInputField = new QLineEdit(parent);
+    passwordInputField2 = new QLineEdit(parent);
+    submitButton = new QPushButton(parent);
+    showPasswordButton = new QPushButton(parent);
+
+    // clang-format off
+    changePasswordParameters listenerDialogParams {
+        .showPasswordButton = showPasswordButton,
+        .passwordInputField = passwordInputField,
+        .passwordInputField2 = passwordInputField2,
+        .settings = settings
+    };
+    listenerDialog->onChangePassword(listenerDialogParams);
+
+    const int LANGUAGE_TYPE = settings->value(LANGUAGE_SETTING).toInt();
+    const auto TOOLTIP_TEXT = LANGUAGE_TYPE ? PASSWORD_FIELD_TOOLTIP_VI : PASSWORD_FIELD_TOOLTIP_EN;
+    const auto SUBMIT_BUTTON_TEXT = LANGUAGE_TYPE ? PASSWORD_SUBMIT_BUTTON_VI : PASSWORD_SUBMIT_BUTTON_EN;
+    const auto SHOW_PASSWORD_BUTTON_TEXT = LANGUAGE_TYPE ? PASSWORD_SHOW_BUTTON_VI : PASSWORD_SHOW_BUTTON_EN;
+    // clang-format on
+
+    passwordInputField->setToolTip(TOOLTIP_TEXT);
+    passwordInputField->setEchoMode(QLineEdit::Password);
+
+    passwordInputField2->setToolTip(TOOLTIP_TEXT);
+    passwordInputField2->setEchoMode(QLineEdit::Password);
+
+    submitButton->setText(SUBMIT_BUTTON_TEXT);
+    showPasswordButton->setText(SHOW_PASSWORD_BUTTON_TEXT);
+
+    layout->addWidget(passwordInputField, 0, 0);
+    layout->addWidget(passwordInputField2, 1, 0);
+    layout->addWidget(showPasswordButton, 0, 1);
+    layout->addWidget(submitButton, 1, 1);
+    parent->setLayout(layout);
+}
