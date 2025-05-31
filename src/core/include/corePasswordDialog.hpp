681aefc009bbaa82272dacfeca8f0533d3dc7672
Reim-developer <contact.kaxtr@gmail.com>
2025-06-01 01:38:19 +0700

    Add CorePasswordDialog class with password handling structures and methods

---
+++ src/core/include/corePasswordDialog.hpp
@@ -0,0 +1,38 @@
+#ifndef CORE_PASSWORD_DIALOG_HPP
+#define CORE_PASSWORD_DIALOG_HPP
+
+#include <QPushButton>
+#include <QLineEdit>
+#include <QSettings>
+#include <QDialog>
+#include <functional>
+
+using std::function;
+
+namespace zclipboard::core {
+
+typedef struct {
+    QLineEdit *passwordInputField;
+    QLineEdit *passwordInputField2;
+    QSettings *settings;
+    QDialog *parent;
+} SubmitPasswordParams;
+
+typedef struct {
+    QLineEdit *passwordInputField;
+    QLineEdit *passwordInputField2;
+    QSettings *settings;
+    QPushButton *showPasswordButton;
+} ShowPasswordParams;
+
+class CorePasswordDialog {
+   private:
+    void showPasswordMismatchDialog(QSettings *settings, QDialog *parent);
+
+   public:
+    function<void()> addSubmitPasswordListener(const SubmitPasswordParams &params);
+    function<void()> addShowPasswordListener(const ShowPasswordParams &params);
+};
+}  // namespace zclipboard::core
+
+#endif  // CORE_PASSWORD_DIALOG_HPP
