eeba1430d2bbe4646babb82e8fa108fe4ac1878d
Reim-developer <contact.kaxtr@gmail.com>
2025-06-01 01:38:16 +0700

    Implement password mismatch dialog with localized messages

---
+++ src/core/corePassswordDialog.cc
@@ -0,0 +1,55 @@
+#include "include/corePasswordDialog.hpp"
+#include "../language/include/language.hpp"
+#include "../zUtils/include/settings.hpp"
+#include <QMessageBox>
+
+using std::function;
+using zclipboard::core::CorePasswordDialog;
+using zclipboard::core::SubmitPasswordParams;
+
+void CorePasswordDialog::showPasswordMismatchDialog(QSettings *settings, QDialog *parent) {
+    const auto LANGUAGE_TYPE = settings->value(LANGUAGE_SETTING).toInt();
+    const auto MSG_TITLE = LANGUAGE_TYPE ? DIALOG_ERROR_TITLE_VI : DIALOG_ERROR_TITLE_EN;
+    const auto MSG_TEXT = LANGUAGE_TYPE ? PASSWORD_MISMATCH_VI : PASSWORD_MISMATCH_EN;
+
+    QMessageBox::information(parent, MSG_TITLE, MSG_TEXT);
+}
+
+function<void()> CorePasswordDialog::addShowPasswordListener(const ShowPasswordParams &params) {
+    const auto function = [params]() {
+        const auto ECHO_MODE = params.passwordInputField->echoMode();
+        const auto LANGUAGE_TYPE = params.settings->value(LANGUAGE_SETTING).toInt();
+
+        // clang-format off
+        const auto HIDE_PASSWORD_TEXT = LANGUAGE_TYPE ? PASSWORD_HIDE_BUTTON_VI : PASSWORD_HIDE_BUTTON_EN;
+        const auto SHOW_PASSWORD_TEXT =  LANGUAGE_TYPE ? PASSWORD_SHOW_BUTTON_VI : PASSWORD_SHOW_BUTTON_EN;
+        // clang-format on
+
+        if (ECHO_MODE != params.passwordInputField->Password) {
+            params.passwordInputField->setEchoMode(QLineEdit::Password);
+            params.passwordInputField2->setEchoMode(QLineEdit::Password);
+
+            params.showPasswordButton->setText(SHOW_PASSWORD_TEXT);
+        } else {
+            params.passwordInputField->setEchoMode(QLineEdit::Normal);
+            params.passwordInputField2->setEchoMode(QLineEdit::Normal);
+
+            params.showPasswordButton->setText(HIDE_PASSWORD_TEXT);
+        }
+    };
+
+    return function;
+}
+
+function<void()> CorePasswordDialog::addSubmitPasswordListener(const SubmitPasswordParams &params) {
+    const auto function = [this, params]() {
+        const auto passwordFieldValue1 = params.passwordInputField->text();
+        const auto passswordFieldValue2 = params.passwordInputField2->text();
+
+        if (passwordFieldValue1 != passswordFieldValue2) {
+            showPasswordMismatchDialog(params.settings, params.parent);
+        }
+    };
+
+    return function;
+}
