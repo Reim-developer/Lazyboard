35d510937e8a3efc3a76e87f96f4a6e9abad9494
Reim-developer <contact.kaxtr@gmail.com>
2025-05-31 16:04:11 +0700

    Remove QWidget* parameter in void function onChangePassword, use QDialog* instead for object GUI parent Add macro Q_OBJECT, and extend class QObject for GUI class GuiDialog

--- src/zGui/include/dialog.hpp
+++ src/zGui/include/dialog.hpp
@@ -1,15 +1,18 @@
 #ifndef GUI_DIALOG_HPP
 #define GUI_DIALOG_HPP
-#include <QWidget>
+#include <QDialog>
+#include <QObject>
 
 namespace zclipboard::zGui {
-class GuiDialog {
+class GuiDialog : public QObject {
+    Q_OBJECT
+
    private:
     static inline const constexpr int DIALOG_WIDTH = 400;
     static inline const constexpr int DIALOG_HEIGHT = 400;
 
    public:
-    void showInputPassword(QWidget *parent);
+    void showInputPassword(QDialog *parent);
 };
 
 }  // namespace zclipboard::zGui
