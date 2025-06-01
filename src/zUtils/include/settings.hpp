a21b7ce48968c73ba3eff7647c0a2548f336fccd
Reim-developer <contact.kaxtr@gmail.com>
2025-06-01 18:46:37 +0700

    Add PASSWORD_SETTING constant to settings header

--- src/zUtils/include/settings.hpp
+++ src/zUtils/include/settings.hpp
@@ -5,5 +5,6 @@ inline constexpr const char* AUTO_HIDE_SETTING = "settings/auto_hide";
 inline constexpr const char* AUTO_NOTIFICATION_SETTING = "settings/auto_notification";
 inline constexpr const char* LANGUAGE_SETTING = "settings/languages";
 inline constexpr const char* THEME_SETTING = "settings/theme";
+inline constexpr const char* PASSWORD_SETTING = "settings/password";
 
 #endif  // SETTINGS_HPP
