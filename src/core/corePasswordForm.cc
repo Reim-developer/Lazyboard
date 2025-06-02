d98970c3385744e19e145b765bb560b72c68083a
Reim-developer <contact.kaxtr@gmail.com>
2025-06-02 20:18:23 +0700

    Refactor addPasswordFormListener to accept QLineEdit parameter for password input

--- src/core/corePasswordForm.cc
+++ src/core/corePasswordForm.cc
@@ -1,12 +1,17 @@
 #include "include/corePasswordForm.hpp"
+#include <QDebug>
+#include <string>
 
 using std::function;
+using std::string;
 using zclipboard::core::CorePasswordForm;
 
-auto CorePasswordForm::addPasswordFormListener() -> function<void()> {
-    const auto function = []() {
-
+auto CorePasswordForm::addPasswordFormListener(QLineEdit *passwordLine) -> function<void()> {
+    // clang-format off
+    const auto function = [passwordLine]() { 
+        qDebug() << passwordLine->text();
     };
+    // clang-format on
 
     return function;
 }
