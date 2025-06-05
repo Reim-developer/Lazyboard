b4fd47c39c9b63168b59db97e9185d71917e8bd5
Reim-developer <contact.kaxtr@gmail.com>
2025-06-05 18:11:28 +0700

    Add new parameter *QDialog to addPasswordFormListener

--- src/core/include/corePasswordForm.hpp
+++ src/core/include/corePasswordForm.hpp
@@ -1,15 +1,17 @@
 #ifndef CORE_PASSWORD_FORM_HPP
 #define CORE_PASSWORD_FORM_HPP
-
+#include <QDialog>
 #include <QLineEdit>
 #include <functional>
+#include "enum.hpp"
 
 using std::function;
+using zclipboard::core::HashState;
 
 namespace zclipboard::core {
 class CorePasswordForm {
    public:
-    auto addPasswordFormListener(QLineEdit *passwordLine) -> function<void()>;
+    function<void()> addPasswordFormListener(QLineEdit *passwordLine, QDialog *parent);
 };
 }  // namespace zclipboard::core
 
