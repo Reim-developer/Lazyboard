eceed099380ceacd08879423987810f08bcb8f76
Reim-developer <contact.kaxtr@gmail.com>
2025-06-07 11:34:54 +0700

    Refactor setLoginFormGui to improve dialog handling and integrate CorePasswordForm

--- src/preload/password.cc
+++ src/preload/password.cc
@@ -4,6 +4,7 @@
 #include "../zUtils/include/zUtils.hpp"
 #include "../language/include/language.hpp"
 #include "../listener/include/passwordFormListener.hpp"
+#include "../core/include/corePasswordForm.hpp"
 #include <QSettings>
 #include <QDialog>
 #include <memory>
@@ -11,7 +12,6 @@
 #include <QGridLayout>
 #include <QPushButton>
 #include <QLabel>
-#include <QSpacerItem>
 
 // clang-format off
 /*
@@ -26,16 +26,18 @@
 
 using std::make_unique;
 using std::unique_ptr;
+using zclipboard::core::CorePasswordForm;
 using zclipboard::listener::PasswordFormListener;
 using zclipboard::preload::PreloadPassword;
 
-void PreloadPassword::setLoginFormGui(const QSettings &settings) {
-    unique_ptr<QDialog> dialog = make_unique<QDialog>();
+int PreloadPassword::setLoginFormGui(const QSettings &settings) {
+    dialog = make_unique<QDialog>();
     unique_ptr<QLineEdit> passwordField = make_unique<QLineEdit>();
     unique_ptr<QPushButton> loginButton = make_unique<QPushButton>();
     unique_ptr<QLabel> loginDescription = make_unique<QLabel>();
     unique_ptr<QGridLayout> layout = make_unique<QGridLayout>();
     unique_ptr<PasswordFormListener> passwordFormListener = make_unique<PasswordFormListener>();
+    unique_ptr<CorePasswordForm> corePasswordForm = make_unique<CorePasswordForm>();
 
     // clang-format off
     const auto LANGUAGE_TYPE = settings.value(LANGUAGE_SETTING).toInt();
@@ -51,7 +53,11 @@ void PreloadPassword::setLoginFormGui(const QSettings &settings) {
     /*
     * Connect event
     */
-    passwordFormListener->onSubmitPassword(loginButton.get(), passwordField.get(), dialog.get());
+    // clang-format off
+    const auto function = corePasswordForm->addPasswordFormListener(passwordField.get(), dialog.get());
+    connect(passwordFormListener.get(), &PasswordFormListener::loginResult, this, &PreloadPassword::onLoginSubmit);
+    passwordFormListener->onSubmitPassword(loginButton.get(), passwordField.get(), dialog.get(), function);
+    // clang-format on
 
     layout->addWidget(loginDescription.get(), 0, 0);
     layout->addWidget(passwordField.get(), 1, 0);
@@ -60,7 +66,32 @@ void PreloadPassword::setLoginFormGui(const QSettings &settings) {
     dialog->setLayout(layout.get());
     dialog->setWindowTitle(FORM_TITLE);
     dialog->setMinimumSize(500, 100);
-    dialog->exec();
+
+    const auto RESULT_CODE = dialog->exec();
+
+    return RESULT_CODE;
+}
+
+void PreloadPassword::onLoginSubmit(HashState hashState) {
+    switch (hashState) {
+        case HashState::HASH_OK:
+            // clang-format off
+            #if defined(Z_DEBUG)
+                qDebug() << "Hash match. Close dialog";
+            #endif
+            // clang-format on
+            dialog->accept();
+
+        /*
+        * Otherwise will be implement in 'CorePasswordForm'
+        */
+        case core::HashState::HASH_MISMATCH:
+        case core::HashState::DELETED:
+        case core::HashState::MODIFIED:
+        case core::HashState::HASH_FILE_NOT_FOUND:
+        case core::HashState::HASH_FILE_EMPTY:
+            break;
+    }
 }
 
 auto PreloadPassword::showLoginForm() -> LoginState {
@@ -81,11 +112,19 @@ auto PreloadPassword::showLoginForm() -> LoginState {
         */
         #if defined(Z_DEBUG)
             qDebug() << "Password setting detect. Show login dialog now.";
-            setLoginFormGui(settings);
+            const auto LOGIN_STATUS = setLoginFormGui(settings);
+
+            if(LOGIN_STATUS == QDialog::Accepted) {
+                return LoginState::LOGIN_OK;
+            } 
 
         #else
 
-            setLoginFormGui(settings);
+            const auto LOGIN_STATUS = setLoginFormGui(settings);
+
+            if(LOGIN_STATUS == QDialog::Accepted) {
+                return LoginState::LOGIN_OK;
+            } 
 
         #endif
         // clang-format on
