22b61142f1c7647a70982584c58b28bae0435e7c
Reim-developer <contact.kaxtr@gmail.com>
2025-07-10 19:41:56 +0700

    Refactor ClearButton class to use smart pointers for memory management and update member variables

--- src/zGui/include/clearButton.hpp
+++ src/zGui/include/clearButton.hpp
@@ -5,19 +5,29 @@
 #include <QObject>
 #include "ztable.hpp"
 #include <QSettings>
+#include "../../lib_memory/include/memory.hpp"
+#include "../../core/include/clear.hpp"
+#include "../../listener/include/clearCacheListener.hpp"
 
 using zclipboard::zGui::ZTable;
+using zclipboard::lib_memory::PtrUnique;
+using zclipboard::core::ClearCoreBuilder;
+using zclipboard::listener::ClearCacheListener;
 
 namespace zclipboard::zGui {
-class ClearButton : public QObject {
-    Q_OBJECT
+class ClearButton  {
+    private:
+        ClearCoreBuilder BuilderCore;
+        ClearCacheListener BuilderFunc;
+
    public:
-    void addClearButton(QGridLayout *layout, ZTable *table);
+    void addClearButton(QGridLayout *layout, ZTable* table);
     QPushButton *getClearButton();
 
    private:
-    QSettings *settings;
-    QPushButton *clearButton;
+
+    PtrUnique<QSettings> settings;
+    PtrUnique<QPushButton> clearButton;
 };
 }  // namespace zclipboard::zGui
 
