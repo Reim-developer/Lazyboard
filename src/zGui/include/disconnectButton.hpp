5867857af2dfb749cde2c98196b5da66e002c79f
Reim-developer <contact.kaxtr@gmail.com>
2025-07-11 22:16:43 +0700

    - Removed DisconnectButtonWidget struct - removed QOBJECT macro in class DisconnectButton - Removed member: 'QSettings, QPushButton, QTcpServer' in private field, refactor & removed unused include.

--- src/zGui/include/disconnectButton.hpp
+++ src/zGui/include/disconnectButton.hpp
@@ -1,38 +1,31 @@
 #ifndef DISCONNECT_BUTTON_HPP
 #define DISCONNECT_BUTTON_HPP
-#include <QObject>
 #include <QPushButton>
 #include <QGridLayout>
-#include <QtNetwork/QTcpServer>
-#include <QSettings>
+#include <QMainWindow>
+#include "../../lib_memory/include/memory.hpp"
 #include "getButton.hpp"
+#include "../../core/include/CoreDisconnect.hpp"
+#include "../../listener/include/ListenerDisconnect.hpp"
 
 using zclipboard::zGui::GetButton;
+using zclipboard::lib_memory::PtrUnique;
+using zclipboard::core::CoreDisconnect;
+using zclipboard::listener::ListenerDisconnect;
 
 namespace zclipboard::zGui {
 
-struct DisconnectButtonWidget {
-    QWidget *parent;
-    QGridLayout *layout;
-    QPushButton *button;
-    GetButton *getButton;
-};
+class DisconnectButton {
+    private:
+        PtrUnique<QSettings> settings;
+        CoreDisconnect Builder;
+        ListenerDisconnect BuilderFunc;
+        PtrUnique<QPushButton> disconnectButton;
 
-class DisconnectButton : public QObject {
-    Q_OBJECT
-
-   public:
-    void addDisconnectButton(const DisconnectButtonWidget &params);
-    QPushButton *getDisconnectButton();
-
-   private:
-    void disconnectFromHost(const DisconnectButtonWidget &params);
-
-   private:
-    QSettings *settings;
-    QPushButton *button;
-    QTcpServer *server;
-};
+    public:
+        void addDisconnectButton(QMainWindow *parent, GetButton *getButton, QGridLayout *layout);
+        QPushButton *getDisconnectButton();
+    };
 }  // namespace zclipboard::zGui
 
 #endif  // DISCONNECT_BUTTON_HPP
