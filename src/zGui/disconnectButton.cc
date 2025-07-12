35ebf0834e208297a881fd3f740a382a8ed5ac2a
Reim-developer <contact.kaxtr@gmail.com>
2025-07-12 12:50:58 +0700

    Fix macro error in compiler step on Windows.

--- src/zGui/disconnectButton.cc
+++ src/zGui/disconnectButton.cc
@@ -3,6 +3,7 @@
 #include <QPushButton>
 #include <QMessageBox>
 #include <QStringLiteral>
+#include <QtGlobal>
 #include "../core/include/CoreDisconnect.hpp"
 #include "../zUtils/include/config.hpp"
 
@@ -10,9 +11,19 @@ using zclipboard::zGui::DisconnectButton;
 using zclipboard::listener::DisconnectImpl;
 
 void DisconnectButton::addDisconnectButton(QMainWindow *parent, GetButton *getButton, QGridLayout *layout) {
-    MAKE_SMART_PTR(QPushButton, disconnectButton);
-    MAKE_SMART_PTR(QSettings, settings, (AUTHOR_NAME, APP_NAME));
-    ADD_LAYOUT_TO(layout, disconnectButton.get(), 0, 4);
+    /*
+    * That macro only work in Linux/Darwin.
+    */
+    #if !defined (_WIN32)
+        MAKE_SMART_PTR(QPushButton, disconnectButton);
+        MAKE_SMART_PTR(QSettings, settings, (AUTHOR_NAME, APP_NAME));
+        ADD_LAYOUT_TO(layout, disconnectButton.get(), 0, 4);
+    #endif
+
+    disconnectButton = MakePtr<QPushButton>();
+    settings = MakePtr<QSettings>(AUTHOR_NAME, APP_NAME);
+    layout->addWidget(disconnectButton.get(), 0, 4);
+     
 
     const auto Function = BuilderFunc
         .   StartBuild()
