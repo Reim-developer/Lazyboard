c5dbd4f22c974e11d135bb352755b9e7b1e2a029
Reim-developer <contact.kaxtr@gmail.com>
2025-07-14 12:16:44 +0700

    Implement NetworkState class for managing server and socket connections

---
+++ Source/Network/NetworkState.cc
@@ -0,0 +1,65 @@
+#include "Include/NetworkState.hpp"
+#include "../Utils/Include/Meta_Macro.hpp"
+
+using ZClipboard::Network::NetworkState;
+
+NetworkState::NetworkState() {
+    #if !defined (_WIN32)
+
+        MAKE_SMART_PTR(QTcpServer, server);
+        MAKE_SMART_PTR(QTcpSocket, socket);
+
+    #else
+
+        server = MakePtr<QTcpServer>();
+        socket = MakePtr<QTcpSocket>();
+
+    #endif
+}
+
+QTcpServer *NetworkState::GetServer() {
+    if(!server) {
+        #if !defined(_WIN32)
+
+            MAKE_SMART_PTR(QTcpServer, server);
+
+        #else
+
+            server = MakePtr<QTcpServer>();
+
+        #endif
+    }
+
+    return server.get();
+}
+
+QTcpSocket *NetworkState::GetSocket() {
+    if(!socket) {
+          #if !defined(_WIN32)
+
+            MAKE_SMART_PTR(QTcpSocket, socket);
+
+        #else
+
+            socket = MakePtr<QTcpServer>();
+
+        #endif
+    }
+
+    return socket.get();
+}
+
+void NetworkState::ResetServer() {
+    if(server) {
+        server->close();
+        server->deleteLater();
+        server = nullptr;
+    }
+}
+
+void NetworkState::ResetSocket() {
+    if(socket) {
+        socket->deleteLater();
+        socket = nullptr;
+    }
+}
