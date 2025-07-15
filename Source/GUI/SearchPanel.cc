c91a37914c6ce739e4212a742d9264afc9da0534
Reim-developer <contact.kaxtr@gmail.com>
2025-07-15 13:38:07 +0700

    - Removed unused include: <QVariant> <QList> + Add include <QString> + Add new Function chain `StartBuild` & template function `WithAndThen`

--- Source/GUI/SearchPanel.cc
+++ Source/GUI/SearchPanel.cc
@@ -1,24 +1,49 @@
 #include "Include/SearchPanel.hpp"
 #include <QLineEdit>
-#include <QVariant>
-#include <QList>
 #include <QTimer>
+#include <QString>
 #include "Include/TableView.hpp"
 
 using ZClipboard::GUI::SearchArea;
 
-void SearchArea::SetupSearchPanel(const SearchPanelWidget &search) {
-    zLineEdit = new QLineEdit(search.window);
-    zLineEdit->setPlaceholderText("Search...");
-    search.layout->addWidget(zLineEdit, 0, 0);
+SearchArea *SearchArea::StartBuild() {
+    Utils::MakeSmartPtr<Implement>(Impl);
 
-    auto zModel = search.tableView->GetTableModel();
-    
-    connect(zLineEdit, &QLineEdit::textChanged, this, [zModel](const QString &text) {
-        QTimer::singleShot(300, zModel, [zModel, text]() { zModel->filterItems(text); });
-    });
+    return this;
+}
+
+SearchArea *SearchArea::WhenDone() {
+    return this;
 }
 
 QLineEdit *SearchArea::GetSearchPanel() {
-    return zLineEdit;
+    auto searchLine = this
+        ->  Impl
+        ->  tookit
+        ->  GetSearchArea();
+
+    return searchLine;
+}
+
+void SearchArea::SetupSearchPanel() {
+    auto searchLine = this->GetSearchPanel();
+
+    auto windowLayout = this
+        ->  Impl
+        ->  layout;
+
+    auto tableView = this
+        ->  Impl
+        ->  tableView;
+
+    searchLine->setPlaceholderText("Search...");
+    windowLayout->addWidget(searchLine, 0, 0);
+
+    auto modelTable = tableView->GetTableModel();
+    
+    QObject::connect(searchLine, &QLineEdit::textChanged, [modelTable](const QString &text) {
+        QTimer::singleShot(300, modelTable, [modelTable, text]() { 
+            modelTable->filterItems(text); 
+        });
+    });
 }
