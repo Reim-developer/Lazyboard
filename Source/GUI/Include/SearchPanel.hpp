800b7628a24c187c09560b375e2ed219b12216d2
Reim-developer <contact.kaxtr@gmail.com>
2025-07-15 13:42:47 +0700

    - Removed unused include: <QObject>, <QString>, <QWidget> - Removed field `window` with type `QWidget`, use `QMainWindow` instead. - Removed Q_OBJECT macro + Add new template chain method WithAndThen + Add new two chain methods `StartBuild` and `WhenDone`

--- Source/GUI/Include/SearchPanel.hpp
+++ Source/GUI/Include/SearchPanel.hpp
@@ -1,34 +1,50 @@
 #ifndef zSearchPanel_HPP
 #define zSearchPanel_HPP
-#include <QObject>
-#include <QString>
-#include <QWidget>
+#include <QMainWindow>
 #include <QLineEdit>
 #include <QGridLayout>
 #include "TableModel.hpp"
 #include "TableView.hpp"
 #include "../../Utils/Include/Namespace_Macro.hpp"
+#include "../Toolkit/Include/Components_Toolkit.hpp"
+#include "../../Utils/Include/Meta_Macro.hpp"
+#include "../../Utils/Include/Utils.hpp"
+#include "../../Lib_Memory/Include/Memory.hpp"
 
 using ZClipboard::GUI::TableView;
 using ZClipboard::GUI::TableModel;
+using ZClipboard::GUI::Toolkit::ComponentsToolkit;
+using ZClipboard::Lib_Memory::PtrUnique;
+using ZClipboard::AppUtils::Utils;
 
 GUI_NAMESPACE
-
-    struct SearchPanelWidget {
-        QWidget *window;
+    struct SearchAreaImpl {
+        QMainWindow *window;
         QGridLayout *layout;
         TableView *tableView;
+        ComponentsToolkit *tookit;
     };
 
-    class SearchArea : public QObject {
-        Q_OBJECT
-
-    public:
-        void SetupSearchPanel(const SearchPanelWidget &search);
-        QLineEdit *GetSearchPanel();
+    class SearchArea {
 
     private:
-        QLineEdit *zLineEdit;
+        PtrUnique<SearchAreaImpl> Impl;
+        using Implement = SearchAreaImpl;
+
+    public:
+        SearchArea *StartBuild();
+        
+        CLASS_BUILD(T, V)
+        SearchArea *WithAndThen(T Implement::*member, V &&value) {
+            Impl.get()->*member = FORWARD(V, value);
+
+            return this;
+        }
+
+        SearchArea *WhenDone();
+
+        void SetupSearchPanel();
+        QLineEdit *GetSearchPanel();
     };
 
 END_NAMESPACE
