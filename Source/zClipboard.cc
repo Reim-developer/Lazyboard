3064f5b1e2989559ec68587f4f803e5f38c4d45d
Reim-developer <contact.kaxtr@gmail.com>
2025-07-17 00:41:18 +0700

    - Removed unused include and using. - Implement class Theme_Impl to hot-reload theme changed and load user default/setting theme.

--- Source/zClipboard.cc
+++ Source/zClipboard.cc
@@ -1,37 +1,29 @@
 #include "GUI/Include/Window.hpp"
-#include "Preload/Include/PreloadTheme.hpp"
+#include "Imlements/Include/Theme_Impl.hpp"
 #include "Preload/Include/PreloadPassword.hpp"
 #include "Core/Include/Enum.hpp"
 #include <QApplication>
-#include <memory>
 
-using std::make_unique;
-using std::unique_ptr;
 using ZClipboard::Core::LoginState;
-using ZClipboard::Preload::Application;
 using ZClipboard::Preload::PreloadPassword;
+using ZClipboard::Implements::Theme_Impl;
 using ZClipboard::GUI::AppMainWindow;
 
 int main(int argc, char *argv[]) {
-    unique_ptr<Application> preloadApplication = make_unique<Application>();
-    unique_ptr<PreloadPassword> preloadPassword = make_unique<PreloadPassword>();
+    PtrUnique<QApplication> application = MakePtr<QApplication>(argc, argv);
+    PtrUnique<Theme_Impl> zClipboard_Impl = MakePtr<Theme_Impl>();
+    PtrUnique<PreloadPassword> preloadPassword = MakePtr<PreloadPassword>();
 
-    const bool IS_DEFAULT_THEME = preloadApplication->loadDefaultTheme();
-
-    QApplication application(argc, argv);
-    if (!IS_DEFAULT_THEME) {
-        preloadApplication->loadSettingTheme(application);
-    }
-
-    preloadApplication->onSettingThemeChanged(application);
     const auto LOGIN_STATUS = preloadPassword->showLoginForm();
 
-    qDebug() << static_cast<int>(LOGIN_STATUS);
     if (LoginState::LOGIN_OK == LOGIN_STATUS || LoginState::LOGIN_NOT_SET == LOGIN_STATUS) {
         AppMainWindow window;
+        
+        zClipboard_Impl
+            ->  StartBuild()
+            ->  ThemeImplement(application.get());
         window.show();
-
-        return application.exec();
+        return application->exec();
     }
 
     return static_cast<int>(LoginState::LOGIN_FAILED);
