f22e5ad72ad71fb68d95269751d9456d13fc558e
Reim-developer <contact.kaxtr@gmail.com>
2025-07-17 14:52:30 +0700

    Defined function `SetupAppLayout` for manager app layout.

---
+++ Source/Helper_Implements/Include/LayoutManager_Impl_Helper.hpp
@@ -0,0 +1,9 @@
+#ifndef LayoutManager_Impl_Helper
+#define LayoutManager_Impl_Helper
+#include "../../GUI/Toolkit/Include/Components_Toolkit.hpp"
+#include <QGridLayout>
+using Toolkit = ZClipboard::GUI::Toolkit::ComponentsToolkit;
+
+void SetupAppLayout(Toolkit *tookit, QGridLayout *layout);
+
+#endif // LayoutManager_Impl_Helper
