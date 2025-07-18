a60e57d82d90467df5336877664899d399e24af9
Reim-developer <contact.kaxtr@gmail.com>
2025-07-19 00:19:49 +0700

    Added test case with class object

--- tests/nullptr_assert/Source/main.cc
+++ tests/nullptr_assert/Source/main.cc
@@ -1,13 +1,23 @@
 #include "../../../Source/Utils/Include/AssertNullPtr.hpp"
+#include <stdio.h>
+
+class myObject {
+    public:
+        int myField;
+};
 
 int main() {
+    myObject *my = new myObject();
+    delete my;
+    my = nullptr;
 
     int *myPtr = nullptr;
     int notPtrValue;
     /*
     * Will failed.
     */
-    AssertContext{}.RequireNonNullPtr(myPtr);
+    AssertContext{}.RequireNonNullPtr(my);
+    printf("%d", my->myField);
 
     return 0;
 }
