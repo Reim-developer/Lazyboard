221d67e1f7efd6042f0bc74246aa67c374d6536b
Reim-developer <contact.kaxtr@gmail.com>
2025-07-18 16:35:34 +0700

    Refactor DisconnectButton methods for improved clarity and consistency

--- Source/GUI/DisconnectButton.cc
+++ Source/GUI/DisconnectButton.cc
@@ -4,40 +4,38 @@
 #include <QtGlobal>
 #include "../Core/Include/CoreDisconnect.hpp"
 #include "../Utils/Include/Config.hpp"
+#include "Include/GetConnectButton.hpp"
 
 using ZClipboard::Listener::DisconnectImpl;
 using ZClipboard::GUI::DisconnectButton;
 
-void DisconnectButton::addDisconnectButton(QMainWindow *parent, GetButton *getButton, QGridLayout *layout) {
-    /*
-    * That macro only work in Linux/Darwin.
-    */
-    #if !defined (_WIN32)
-        MAKE_SMART_PTR(QPushButton, disconnectButton);
-        MAKE_SMART_PTR(QSettings, settings, (AUTHOR_NAME, APP_NAME));
-        ADD_LAYOUT_TO(layout, disconnectButton.get(), 0, 4);
+#define __SELF__ DisconnectButton
+#define __TOOKIT__ ComponentsToolkit
+#define __WINDOW__ QMainWindow
+#define __IMPL__ DisconnectImpl
 
-    #else
-        disconnectButton = MakePtr<QPushButton>();
-        settings = MakePtr<QSettings>(AUTHOR_NAME, APP_NAME);
-        layout->addWidget(disconnectButton.get(), 0, 4);
-    #endif
+__SELF__ *__SELF__::UseConnectButton(GetButton *button) {
+    this->getButton = button;
+
+    return this;
+}
+
+void __SELF__::SetupDisconnectButton(__TOOKIT__ *toolkit, __WINDOW__ *window) {
+    Utils::MakeSmartPtr<QSettings>(settings, AUTHOR_NAME, APP_NAME);
+    auto disconnectButton = toolkit->GetDisconnectButton();
 
     const auto Function = BuilderFunc
         .   StartBuild()
-        ->  WithAndThen(&DisconnectImpl::windowParent, parent)
-        ->  WithAndThen(&DisconnectImpl::setting, settings.get())
-        ->  WithAndThen(&DisconnectImpl::getButton, getButton)
+        ->  WithAndThen(&__IMPL__::setting, settings.get())
+        ->  WithAndThen(&__IMPL__::getButton, getButton)
+        ->  WithAndThen(&__IMPL__::toolkit, toolkit)
+        ->  WithAndThen(&__IMPL__::windowParent, window)
         ->  WhenDone()
         ->  TryGetListener();
 
 
     Builder
-        .   StartBuild(disconnectButton.get())
+        .   StartBuild(disconnectButton)
         ->  WhenDone()
         ->  AddListener(Function);
-}
-
-QPushButton *DisconnectButton::getDisconnectButton() {
-    return disconnectButton.get();
 }
