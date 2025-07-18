b57a6c74483de212bcf3fe28e14f111ac693d879
Reim-developer <contact.kaxtr@gmail.com>
2025-07-18 16:35:31 +0700

    Refactor ClearButton setup methods to improve clarity and reduce dependencies

--- Source/GUI/ClearButton.cc
+++ Source/GUI/ClearButton.cc
@@ -1,54 +1,37 @@
 #include "Include/ClearButton.hpp"
-#include <QGridLayout>
-#include <QPushButton>
 #include "../Utils/Include/Config.hpp"
-#include "../Lib_Memory/Include/Memory.hpp"
 #include "../Listener/Include/ListenerClearCache.hpp"
-#include <QSettings>
 #include "../Core/Include/CoreClearCache.hpp"
 #include "Include/TableView.hpp"
-#include "../Utils/Include/Meta_Macro.hpp"
 #include "../Utils/Include/Utils.hpp"
-#include "Toolkit/Include/Layout_Tookit.hpp"
 
 using ZClipboard::Lib_Memory::PtrUnique;
 using ZClipboard::GUI::ClearButton;
 using ZClipboard::Listener::ListenerCacheImpl;
 using ZClipboard::AppUtils::Utils;
-using ZClipboard::GUI::Toolkit::WidgetProperty;
-using ZClipboard::GUI::Toolkit::GridLayoutAdd;
 
-void ClearButton::SetupClearButton(QGridLayout *layout, TableView* tableView) {
-    Utils::MakeSmartPtr<QPushButton>(clearButton);
+void ClearButton::SetupClearButton(QGridLayout *layout, Toolkit *toolkit, TableView *tableView) {
+    auto clearButton = toolkit->GetClearButton();
     Utils::MakeSmartPtr<QSettings>(settings, AUTHOR_NAME, APP_NAME);
     
-    using WgImpl = WidgetProperty;
-    GridLayoutAdd(layout, WgImpl{
-        .widget = clearButton.get(),
-        .row = 0, .column = 2
-    });
-
-    SetupEventListener(tableView);
+    SetupEventListener(clearButton, tableView);
     #if defined (Z_DEBUG)
-        __LOG__
+        __LOGGING_ALL_OBJECTS__(clearButton);
     #endif
 }
 
-void ClearButton::SetupEventListener(TableView *tableView) {
+void ClearButton::SetupEventListener(QPushButton *clearButton, TableView *tableView) {
     using Impl = ListenerCacheImpl;
+
     const auto Function = BuilderFunc
                 .   StartBuild()
-                    ->  WithAndThen(&Impl::button, clearButton.get())
+                    ->  WithAndThen(&Impl::button, clearButton)
                     ->  WithAndThen(&Impl::settings, settings.get())
                     ->  WithAndThen(&Impl::table, tableView)
                     ->  WhenDone()
                     ->  TryGetListener();
 
     BuilderCore
-        .   StartBuild(clearButton.get())
+        .   StartBuild(clearButton)
             ->  ThenAddListener(Function);
-}
-
-QPushButton *ClearButton::getClearButton() {
-    return clearButton.get();
 }
