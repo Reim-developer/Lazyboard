e08c8a2b9efc8d1f4f5375d65bc36a0aea53a013
Reim-developer <contact.kaxtr@gmail.com>
2025-07-20 04:54:43 +0700

    Implement & set the field value of `disconnectButton_Raw` & `connectButton_Raw`

--- Source/Lib_Memory/MainWindow_ObjectManager.cc
+++ Source/Lib_Memory/MainWindow_ObjectManager.cc
@@ -24,12 +24,16 @@ Object *Self::SetupObjectData() {
     auto searchArea_Raw = this->SetupFieldData<SearchArea>(searchArea);
     auto notification_Raw = this->SetupFieldData<NotificationCore>(notificationCore);
     auto systemTray_Raw = this->SetupFieldData<SystemTray>(systemTray);
+    auto disconnectButton_Raw = this->SetupFieldData<DisconnectButton>(disconnectButton);
+    auto connectButton_Raw = this->SetupFieldData<ConnectButton>(connectButton);
 
     object->tableView_Component = tableView_Raw;
     object->clearButton_Component = clearButton_Raw;
     object->searchArea_Component = searchArea_Raw;
     object->notificationCore_Component = notification_Raw;
     object->systemTray_Component = systemTray_Raw;
+    object->disconnectButton_Component = disconnectButton_Raw;
+    object->connectButton_Component = connectButton_Raw;
 
     #if defined(Z_DEBUG)
         AssertContext{}.RequireNonNullPtr(tableView_Raw);
@@ -37,12 +41,16 @@ Object *Self::SetupObjectData() {
         AssertContext{}.RequireNonNullPtr(object.get());
         AssertContext{}.RequireNonNullPtr(object->notificationCore_Component);
         AssertContext{}.RequireNonNullPtr(object->systemTray_Component);
+        AssertContext{}.RequireNonNullPtr(object->disconnectButton_Component);
+        AssertContext{}.RequireNonNullPtr(object->connectButton_Component);
 
         LogContext{}.LogDebug(&tableView_Raw);
         LogContext{}.LogDebug(&object->tableView_Component);
         LogContext{}.LogDebug(object.get());
         LogContext{}.LogDebug(&object->notificationCore_Component);
         LogContext{}.LogDebug(&object->systemTray_Component);
+        LogContext{}.LogDebug(&object->disconnectButton_Component);
+        LogContext{}.LogDebug(&object->connectButton_Component);
     #endif
 
     return object.get();
