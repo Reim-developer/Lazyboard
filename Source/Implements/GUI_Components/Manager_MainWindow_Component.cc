127be59fdba6087d73fa3b931f7d88fe9ded5716
Reim-developer <contact.kaxtr@gmail.com>
2025-07-19 23:03:16 +0700

    Refactor: Enhance MainWindowComponentsManager methods for improved clarity and structure

--- Source/Implements/GUI_Components/Manager_MainWindow_Component.cc
+++ Source/Implements/GUI_Components/Manager_MainWindow_Component.cc
@@ -3,11 +3,53 @@
 using ZClipboard::Implements::MainWindowComponentsManager;
 
 using Self = MainWindowComponentsManager;
+using Window = QMainWindow;
 
-void Self::Render_MainWindow_GUI(Object *object, Toolkit *toolkit, QIcon appIcon) {
+Self *Self::StartBuild() {
+    if(!Impl) {
+        Utils::MakeSmartPtr<DataImpl>(Impl);
+    }
+
+    return this;
+}
+
+Object *Self::GetObject() {
+    return this
+        ->  Impl
+        ->  object;
+}
+
+QIcon *Self::GetAppIcon() {
+    return this
+        ->  Impl
+        ->  appIcon;
+}
+
+Window *Self::GetWindow() {
+    return this
+        ->  Impl
+        ->  window;
+}
+
+Toolkit *Self::GetToolkit() {
+    return this
+        ->  Impl
+        ->  toolkit;
+}
+
+Self *Self::WhenDone() {
+    return this;
+}
+
+void Self::Render_MainWindow_GUI() {
+    auto object = this->GetObject();
+    auto toolkit = this->GetToolkit();
+    auto appIcon = this->GetAppIcon();
+    auto window = this->GetWindow();
+
+    TableViewComponentImpl(object, toolkit);
     ClearButtonComponentImpl(object, toolkit);
     SearchAreaComponentImpl(object, toolkit);
-    TableViewComponentImpl(object, toolkit);
     NotificationComponentImpl(object, toolkit);
-    SystemTrayComponentImpl(object, toolkit, appIcon);
+    SystemTrayComponentImpl(window, object, toolkit, *appIcon);
 }
