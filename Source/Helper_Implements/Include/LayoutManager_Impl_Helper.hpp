8bcc01c740098536859fb3418b47796ec35faaa9
Reim-developer <contact.kaxtr@gmail.com>
2025-07-20 22:39:25 +0700

    Fix naming conflict in `LayoutManager_Impl_Helper.hpp` for implement new naming convention

--- Source/Helper_Implements/Include/LayoutManager_Impl_Helper.hpp
+++ Source/Helper_Implements/Include/LayoutManager_Impl_Helper.hpp
@@ -2,8 +2,8 @@
 #define LayoutManager_Impl_Helper
 #include "../../GUI/Toolkit/Include/MainWindow_Components.hpp"
 #include <QGridLayout>
-using Toolkit = ZClipboard::GUI::Toolkit::ComponentsToolkit;
+using ComponentsManager = ZClipboard::GUI::Toolkit::MainWindowComponentsManager;
 
-void SetupAppLayout(Toolkit *tookit, QGridLayout *layout);
+void SetupAppLayout(ComponentsManager *tookit, QGridLayout *layout);
 
 #endif // LayoutManager_Impl_Helper
