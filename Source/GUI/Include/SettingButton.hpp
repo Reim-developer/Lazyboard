d56298f37386fe131365a7454dc182e7382ff098
Reim-developer <contact.kaxtr@gmail.com>
2025-07-27 05:40:48 +0700

    feat: Removed old-deprecated include & using class/struct Commented old-deprecated struct data `SettingWidget` & private field member in class `SettingButton` Added new private method `SetupSettingButton`, with one parameter `Button (QPushButton type alias) *openSettingButton`

--- Source/GUI/Include/SettingButton.hpp
+++ Source/GUI/Include/SettingButton.hpp
@@ -11,38 +11,44 @@
 #include <QComboBox>
 #include <QLabel>
 #include <QMainWindow>
-#include "../../Core/Include/CoreSetting.hpp"
 #include "../../Lib_Memory/Include/Memory.hpp"
 #include "../Toolkit/Include/MainWindow_Components.hpp"
-#include "../../Core/Include/CoreSettingWindow.hpp"
 #include "../../Utils/Include/Namespace_Macro.hpp"
+#include "../../GUI/Windows/Include/SettingWindow.hpp"
+#include "../../Listener/Include/ListenerOpenSettingWindow.hpp"
 
-using ZClipboard::Core::SettingCore;
 using ZClipboard::GUI::Toolkit::MainWindowComponentsManager;
+using ZClipboard::GUI::Windows::SettingWindowManager;
 using ZClipboard::Core::CoreSettingWindow;
+using ZClipboard::Listener::ListenerOpenSettingWindow;
 using ZClipboard::Lib_Memory::PtrUnique;
 
 GUI_NAMESPACE
 
-    struct SettingWidget {
-        QCheckBox *checkbox;
-        QDialog *dialog;
-        QSettings *settings;
-        QLabel *languageDescription;
-        QGridLayout *layout;
-        QComboBox *comboBox;
-    };
+    // struct SettingWidget {
+    //     QCheckBox *checkbox;
+    //     QDialog *dialog;
+    //     QSettings *settings;
+    //     QLabel *languageDescription;
+    //     QGridLayout *layout;
+    //     QComboBox *comboBox;
+    // };
 
     class SettingButton : public QObject {
         Q_OBJECT
 
     private:
         using Window = QMainWindow;
-        using MainWindowComponents = MainWindowComponentsManager;
+        using Components = MainWindowComponentsManager;
+        using Listener = ListenerOpenSettingWindow;
+        using Button = QPushButton;
+
+    private:
+        void SetupListener(Button *openSettingButton);
 
     public:
-        void SetupSettingButton(Window *window, MainWindowComponents *toolkit);
-        QPushButton *getSettingButton();
+        void SetupSettingButton(Window *window, Components *components);
+        // QPushButton *getSettingButton();
 
     private:
         // void showSettingDialog(QMainWindow *parent);
@@ -52,21 +58,20 @@ GUI_NAMESPACE
         // void addThemeSectionAction();
 
     private:
-        SettingCore *settingCore;
-        PtrUnique<QSettings> settings;
-        PtrUnique<QDialog> dialog;
-        PtrUnique<QGridLayout> layout;
-        PtrUnique<CoreSettingWindow> coreSettingWindow;
-        QPushButton *setPasswordButton;
+        // PtrUnique<QSettings> settings;
+        // PtrUnique<QDialog> dialog;
+        // PtrUnique<QGridLayout> layout;
+        PtrUnique<Listener> listener;
+        // QPushButton *setPasswordButton;
 
-        QLabel *languageDescription;
-        QLabel *themeDescription;
+        // QLabel *languageDescription;
+        // QLabel *themeDescription;
 
-        QComboBox *languageBox;
-        QComboBox *themeBox;
+        // QComboBox *languageBox;
+        // QComboBox *themeBox;
 
-        QCheckBox *autoHideCheckBox;
-        QCheckBox *autoNotificatonCheckBox;
+        // QCheckBox *autoHideCheckBox;
+        // QCheckBox *autoNotificatonCheckBox;
     };
     
 END_NAMESPACE
