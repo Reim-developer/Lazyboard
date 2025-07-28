331e1cb3f83f1b47d6bbb3cc7cd2036fd5c3d912
Reim-developer <contact.kaxtr@gmail.com>
2025-07-29 00:39:44 +0700

    fix: Comment some code blocks for testing

--- scripts/ci_dev_linux.sh
+++ scripts/ci_dev_linux.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 build_dir="build"
-qt_src_dir="qt-src"
+
 qt_build_dir="qt-build"
 qt_install_dir="/opt/qt-static"
-qt_version="6.5.3"
+qt_version="6.9.1"
 release_flags="-DCMAKE_BUILD_TYPE=Release"
 opt_flags="-O3 -march=native -flto -funroll-loops -fomit-frame-pointer -fstrict-aliasing -ftree-vectorize -fvisibility=hidden"
 nproc=$(nproc)
@@ -28,16 +28,14 @@ install_base() {
 }
 
 download_qt_source() {
+    local qt_src_dir="qt-src"
+
     echo "Downloading Qt source..."
     mkdir -p "$qt_src_dir"
     cd "$qt_src_dir" || exit
     
-    if [ -f "qt-everywhere-src-${qt_version}.tar.xz" ]; then
-        echo "Qt source already downloaded."
-    else
-        wget "https://download.qt.io/official_releases/qt/$(echo $qt_version | cut -d. -f1-2)/${qt_version}/single/qt-everywhere-src-${qt_version}.tar.xz"
-        tar xf "qt-everywhere-src-${qt_version}.tar.xz"
-    fi
+    wget "https://download.qt.io/official_releases/qt/$(echo $qt_version | cut -d. -f1-2)/${qt_version}/single/qt-everywhere-src-${qt_version}.tar.xz"
+    tar xf "qt-everywhere-src-${qt_version}.tar.xz"
     
     cd ..
 }
@@ -46,39 +44,40 @@ build_static_qt() {
     echo "Building static Qt..."
     mkdir -p "$qt_build_dir"
     cd "$qt_build_dir" || exit
+    ls
     
-     cmake "$qt_src_dir/qt-everywhere-src-$qt_version" \
-        -G "Ninja" \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$qt_install_dir" \
-        -DCMAKE_C_COMPILER=clang \
-        -DCMAKE_CXX_COMPILER=clang++ \
-        -DCMAKE_CXX_FLAGS="$opt_flags" \
-        -DBUILD_SHARED_LIBS=OFF \
-        -DFEATURE_static=ON \
-        -DFEATURE_static_runtime=ON \
-        -DFEATURE_reduce_exports=ON \
-        -DFEATURE_opengl=desktop \
-        -DFEATURE_system_png=OFF \
-        -DFEATURE_system_jpeg=OFF \
-        -DFEATURE_system_zlib=OFF \
-        -DFEATURE_fontconfig=ON \
-        -DFEATURE_harfbuzz=ON \
-        -DFEATURE_xcb=ON \
-        -DFEATURE_xkbcommon=ON \
-        -DFEATURE_ssl=ON \
-        -DFEATURE_widgets=ON \
-        -DFEATURE_gui=ON \
-        -DFEATURE_network=ON \
-        -DFEATURE_concurrent=ON \
-        -DFEATURE_xml=ON \
-        -DQT_FEATURE_optimize_size=ON \
-        -DQT_FEATURE_pkg_config=OFF \
-        -DQT_FEATURE_separate_debug_info=OFF \
-        -DQT_FEATURE_strip=ON \
-        -DQT_BUILD_EXAMPLES=OFF \
-        -DQT_BUILD_TESTS=OFF \
-        -DQT_SKIP_MODULES=qtwebengine
+    # cmake "$qt_src_dir/qt-everywhere-src-$qt_version" \
+    #     -G "Ninja" \
+    #     -DCMAKE_BUILD_TYPE=Release \
+    #     -DCMAKE_INSTALL_PREFIX="$qt_install_dir" \
+    #     -DCMAKE_C_COMPILER=clang \
+    #     -DCMAKE_CXX_COMPILER=clang++ \
+    #     -DCMAKE_CXX_FLAGS="$opt_flags" \
+    #     -DBUILD_SHARED_LIBS=OFF \
+    #     -DFEATURE_static=ON \
+    #     -DFEATURE_static_runtime=ON \
+    #     -DFEATURE_reduce_exports=ON \
+    #     -DFEATURE_opengl=desktop \
+    #     -DFEATURE_system_png=OFF \
+    #     -DFEATURE_system_jpeg=OFF \
+    #     -DFEATURE_system_zlib=OFF \
+    #     -DFEATURE_fontconfig=ON \
+    #     -DFEATURE_harfbuzz=ON \
+    #     -DFEATURE_xcb=ON \
+    #     -DFEATURE_xkbcommon=ON \
+    #     -DFEATURE_ssl=ON \
+    #     -DFEATURE_widgets=ON \
+    #     -DFEATURE_gui=ON \
+    #     -DFEATURE_network=ON \
+    #     -DFEATURE_concurrent=ON \
+    #     -DFEATURE_xml=ON \
+    #     -DQT_FEATURE_optimize_size=ON \
+    #     -DQT_FEATURE_pkg_config=OFF \
+    #     -DQT_FEATURE_separate_debug_info=OFF \
+    #     -DQT_FEATURE_strip=ON \
+    #     -DQT_BUILD_EXAMPLES=OFF \
+    #     -DQT_BUILD_TESTS=OFF \
+    #     -DQT_SKIP_MODULES=qtwebengine
             # qtwebengine;\
             # qt3d;qt5compat;qtactiveqt;qtcharts;\
             # qtconnectivity;qtdatavis3d;qtdoc;\
