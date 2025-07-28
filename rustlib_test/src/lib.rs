802430937cd5d2df44e373f9ad04b203e3116274
Reim-developer <contact.kaxtr@gmail.com>
2025-07-28 21:46:33 +0700

    fix: Clippy error & added missing `# Safety` sector document

--- rustlib_test/src/lib.rs
+++ rustlib_test/src/lib.rs
@@ -4,6 +4,9 @@ use std::{ffi::CString, os::raw::c_char};
 use std::ptr::null_mut;
 
 #[unsafe(no_mangle)]
+/// # Safety
+/// None. This function just `print` text to your terminal.
+/// Just for test the FFI Rust -> C++
 pub unsafe extern "C" fn greet() {
     println!("Hello World");
 }
@@ -12,11 +15,16 @@ pub unsafe extern "C" fn greet() {
 pub extern "C" fn get_reim_name() -> *mut c_char {
     CString
         ::new("REIM DEVELOPER")
-        .map(|name| name.into_raw())
+        .map(CString::into_raw)
         .unwrap_or(null_mut())
 }
 
 #[unsafe(no_mangle)]
+/// # Safety
+/// Free `c_char`.
+/// 
+/// You cannot use this function to `free` the
+/// same pointer twice. 
 pub unsafe extern "C" fn free_name(c_string: *mut c_char) {
     if !c_string.is_null() {
         unsafe  {
