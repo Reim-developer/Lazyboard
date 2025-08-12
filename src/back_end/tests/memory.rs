f274cb706c03834c7332524405ded66f04b359de
Reim-developer <contact.kaxtr@gmail.com>
2025-08-12 09:30:28 +0700

    Added test for module `memory`

---
+++ src/back_end/tests/memory.rs
@@ -0,0 +1,14 @@
+#[test]
+fn test_memory_mod() {
+    use back_end::utils::memory::raw_free_c_str;
+    use std::ffi::CString;
+    use std::ptr::null_mut;
+
+    let raw_str = CString::new("Reim-developer")
+        .map(|raw| raw.into_raw())
+        .unwrap_or(null_mut());
+
+    unsafe {
+        raw_free_c_str(raw_str);
+    }
+}
