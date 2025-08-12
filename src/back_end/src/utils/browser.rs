e272333d21360deb551a8496234c08a697e60efe
Reim-developer <contact.kaxtr@gmail.com>
2025-08-12 09:30:58 +0700

    Fix, removed `bool` result, use `enum` instead

--- src/back_end/src/utils/browser.rs
+++ src/back_end/src/utils/browser.rs
@@ -1,19 +1,36 @@
-use std::ffi::{CStr, c_char};
+use std::ffi::c_char;
 use webbrowser::open;
+
+use crate::utils::memory::raw_from_ptr;
+
+#[repr(C)]
+pub enum OpenBrowserStatus {
+    Ok,
+    WrapRawCFailed,
+    UrlIsEmpty,
+    OpenBrowserFailed,
+}
 
 /// # Safety
 /// Be careful with raw pointers.
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn open_browser(url: *const c_char) -> bool {
+pub unsafe extern "C" fn raw_open_browser(url: *const c_char) -> OpenBrowserStatus {
     unsafe {
-        let Ok(c_str) = CStr::from_ptr(url).to_str() else {
-            return false;
-        };
+        use OpenBrowserStatus as Status;
 
-        if c_str.is_empty() {
-            return false;
+        let (c_str, is_ok) = raw_from_ptr(url);
+        if !is_ok {
+            return Status::WrapRawCFailed;
         }
 
-        open(c_str).is_ok()
+        if c_str.is_empty() {
+            return Status::UrlIsEmpty;
+        }
+
+        if open(c_str).is_ok() {
+            Status::Ok
+        } else {
+            Status::OpenBrowserFailed
+        }
     }
 }
