5220f9d63494cc6814493056192b132dd13ce693
Reim-developer <contact.kaxtr@gmail.com>
2025-08-14 22:20:26 +0700

    Added test for raw `c_str`

--- tests-ffi/toml_config.cxx
+++ tests-ffi/toml_config.cxx
@@ -1,8 +1,10 @@
 #include <cassert>
 #include <cstdint>
 #include <memory>
+#include <string>
 
 using std::make_unique;
+using std::string;
 using std::uint8_t;
 using std::unique_ptr;
 
@@ -14,20 +16,37 @@ extern "C" enum RawReadAppConfigStatus : uint8_t {
     CONVERT_TO_MUT_FAILED
 };
 
-extern "C" struct RawAppSettings {
+extern "C" typedef struct {
     bool hide_when_closed;
     bool notification;
-};
+} RawAppSettings;
 
-extern "C" RawReadAppConfigStatus raw_exists_config(const char *file_path,
-                                                    RawAppSettings *config_out);
+extern "C" typedef struct {
+    char* background_color;
+    char* foreground_color;
+} RawAppGuiSettings;
+
+extern "C" typedef struct {
+    RawAppSettings raw_app_settings;
+    RawAppGuiSettings raw_app_gui_settings;
+} RawAppConfig;
+
+extern "C" RawReadAppConfigStatus raw_exists_config(const char* file_path,
+                                                    RawAppConfig* config_out);
+
+extern "C" void raw_free_cstr_app_config(RawAppConfig* config);
 
 int main() {
     using Status = RawReadAppConfigStatus;
 
-    const char *path = "demo.toml";
-    struct unique_ptr<RawAppSettings> raw = make_unique<RawAppSettings>();
+    const char* path = "demo.toml";
+    unique_ptr<RawAppConfig> raw = make_unique<RawAppConfig>();
+
     auto result = raw_exists_config(path, raw.get());
+    auto bg_color = string(raw->raw_app_gui_settings.background_color);
+    auto fg_color = string(raw->raw_app_gui_settings.foreground_color);
+
+    raw_free_cstr_app_config(raw.get());
 
     assert(result != Status::UTF_8_ERROR);
     assert(result != Status::PARSE_TOML_FAILED);
@@ -35,10 +54,17 @@ int main() {
     assert(result != Status::CONVERT_TO_MUT_FAILED);
     assert(result == Status::OK);
 
-    assert(raw->hide_when_closed != true);
-    assert(raw->hide_when_closed == false);
-    assert(raw->notification != false);
-    assert(raw->notification == true);
+    assert(raw->raw_app_settings.hide_when_closed != true);
+    assert(raw->raw_app_settings.hide_when_closed == false);
+    assert(raw->raw_app_settings.notification != false);
+    assert(raw->raw_app_settings.notification == true);
+
+    assert(!bg_color.empty());
+    assert(!fg_color.empty());
+    assert(bg_color != "717841xx18");
+    assert(fg_color != "81388181");
+    assert(bg_color == "#2f3136");
+    assert(fg_color == "#ffffff");
 
     return 0;
 }
