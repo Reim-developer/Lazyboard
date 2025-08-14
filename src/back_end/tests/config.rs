1050843c1f2f70adf3cf01f8a0c46ab62a9cfbc5
Reim-developer <contact.kaxtr@gmail.com>
2025-08-14 18:28:25 +0700

    Fix function call signauture in test

--- src/back_end/tests/config.rs
+++ src/back_end/tests/config.rs
@@ -1,9 +1,9 @@
 #[test]
 fn test_config() {
-    use back_end::utils::fs_utils::raw_local_data;
+    use back_end::utils::fs_utils::raw_config_dir;
     use back_end::utils::memory::raw_free_c_str;
 
-    let c_str = raw_local_data();
+    let c_str = raw_config_dir();
     assert!(!c_str.is_null());
 
     unsafe {
