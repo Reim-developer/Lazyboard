c068c60686b2a1fde362312783c7d6077517c0e4
Reim-developer <contact.kaxtr@gmail.com>
2025-08-15 13:54:14 +0700

    Added function wrap to `RawAppConfig`

--- src/ffi/namespace/include/config.hxx
+++ src/ffi/namespace/include/config.hxx
@@ -4,16 +4,29 @@
 
 namespace Lazyboard::ffi {
 
-inline char* config_dir() {
-    char* result = raw_config_dir();
+inline char *config_dir() {
+	char *result = raw_config_dir();
 
-    return result;
+	return result;
 }
+
 inline WriteConfigStatus write_default_config() {
-    auto result = raw_write_default_config();
+	auto result = raw_write_default_config();
 
-    return result;
+	return result;
 }
+
+inline RawReadAppConfigStatus exists_config(const char *path,
+											RawAppConfig *config_out) {
+	auto result = raw_exists_config(path, config_out);
+
+	return result;
+}
+
+inline void free_cstr_app_config(RawAppConfig *config_out) {
+	raw_free_cstr_app_config(config_out);
+}
+
 }  // namespace Lazyboard::ffi
 
-#endif  // CONFIG_HXX
+#endif	// CONFIG_HXX
