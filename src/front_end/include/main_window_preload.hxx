05a6bc16fb17e1267c6a9e6c6f44f7cc4a4f7c21
Reim-developer <contact.kaxtr@gmail.com>
2025-08-15 13:54:40 +0700

    Implement preload, & set window color via TOML configuration

--- src/front_end/include/main_window_preload.hxx
+++ src/front_end/include/main_window_preload.hxx
@@ -3,16 +3,34 @@
 
 #include <qmainwindow.h>
 
+#include <memory>
+#include <string>
+
 #include "../../ffi/raw/config.hxx"
+
+using std::string;
+using std::unique_ptr;
 
 namespace Lazyboard::front_end {
 class MainWindowPreload {
    private:
-    void on_error(WriteConfigStatus status, QMainWindow* main_window);
+	unique_ptr<RawAppConfig> raw_app_config;
+
+   private:
+	void on_gen_default_cfg_error(WriteConfigStatus status,
+								  QMainWindow *main_window);
+
+	void on_read_exists_cfg_error(RawReadAppConfigStatus status,
+								  QMainWindow *main_window);
+
+	string application_config();
+	void set_application_theme(QMainWindow *main_window,
+							   RawAppConfig *app_config);
 
    public:
-    void create_default_config(QMainWindow* main_window);
+	void create_default_config(QMainWindow *main_window);
+	void read_if_exists_config(QMainWindow *main_window);
 };
 }  // namespace Lazyboard::front_end
 
-#endif  // PRELOAD_HXX
+#endif	// PRELOAD_HXX
--- src/front_end/main_window_preload.cxx
+++ src/front_end/main_window_preload.cxx
@@ -1,8 +1,12 @@
 #include "include/main_window_preload.hxx"
 
+#include <qcolor.h>
 #include <qmainwindow.h>
 #include <qmessagebox.h>
+#include <qobject.h>
+#include <qpalette.h>
 
+#include <memory>
 #include <sstream>
 #include <string>
 
@@ -10,78 +14,176 @@
 #include "../ffi/namespace/include/utils.hxx"
 
 #if defined(LAZY_DEBUG)
-#include <qdebug.h>
-#include <qstring.h>
+	#include <iostream>
+using std::cout;
 #endif
 
 using Lazyboard::front_end::MainWindowPreload;
 using Self = MainWindowPreload;
 
+using std::make_unique;
 using std::string;
 using std::stringstream;
 using namespace Lazyboard::ffi;
 
-void Self::on_error(WriteConfigStatus status, QMainWindow* main_window) {
-    switch (status) {
-        case WriteConfigStatus::OK:
-            break;
+void Self::on_gen_default_cfg_error(WriteConfigStatus status,
+									QMainWindow *main_window) {
+	switch (status) {
+		case WriteConfigStatus::OK:
+			break;
 
-        case WriteConfigStatus::CREATE_DIR_FAILED:
-            QMessageBox::critical(main_window, "Error",
-                                  "Could not create config directory");
-            break;
+		case WriteConfigStatus::CREATE_DIR_FAILED:
+			QMessageBox::critical(main_window, "Error",
+								  "Could not create config directory");
 
-        case WriteConfigStatus::GET_DATA_LOCAL_FAILED:
-            QMessageBox::critical(main_window, "Error",
-                                  "Could not get application local data");
-            break;
+			break;
 
-        case WriteConfigStatus::CREATE_FILE_FAILED:
-            QMessageBox::critical(main_window, "Error",
-                                  "Could not create configuration file");
-            break;
+		case WriteConfigStatus::GET_DATA_LOCAL_FAILED:
+			QMessageBox::critical(main_window, "Error",
+								  "Could not get application local data");
+			break;
 
-        case WriteConfigStatus::WRITE_FILE_FAILED:
-            QMessageBox::critical(main_window, "Error",
-                                  "Could not write configuration");
-            break;
+		case WriteConfigStatus::CREATE_FILE_FAILED:
+			QMessageBox::critical(main_window, "Error",
+								  "Could not create configuration file");
+			break;
 
-        case WriteConfigStatus::TOML_TO_STRING_FAILED:
-            QMessageBox::critical(main_window, "Error",
-                                  "Could not convert TOML data to string");
-            break;
-    }
+		case WriteConfigStatus::WRITE_FILE_FAILED:
+			QMessageBox::critical(main_window, "Error",
+								  "Could not write configuration");
+			break;
+
+		case WriteConfigStatus::TOML_TO_STRING_FAILED:
+			QMessageBox::critical(main_window, "Error",
+								  "Could not convert TOML data to string");
+			break;
+	}
 }
 
-void Self::create_default_config(QMainWindow* main_window) {
-    auto config_dir_raw = ffi::config_dir();
+void Self::on_read_exists_cfg_error(RawReadAppConfigStatus status,
+									QMainWindow *main_window) {
+	using Status = RawReadAppConfigStatus;
 
-    stringstream string_stream;
-    string_stream << config_dir_raw << "/Lazyboard"
-                  << "/settings.toml";
-    string config_path = string_stream.str();
+	switch (status) {
+		case Status::READ_OK:
+			break;
 
-    auto is_config_exists = ffi::is_exists_path(config_path.data());
-    ffi::free_c_str(config_dir_raw);
+		case Status::CONVERT_TO_MUT_FAILED:
+			QMessageBox::critical(main_window, "Error",
+								  "Could not convert to *mut c_char");
+			break;
 
-    if (!is_config_exists) {
-        // clang-format off
-      #if defined (LAZY_DEBUG)
-        auto q_string_value = QString::fromStdString(config_path);
+		case Status::PARSE_TOML_FAILED:
+			QMessageBox::critical(main_window, "Error",
+								  "Parse TOML configuration failed");
+			break;
 
-        qDebug() << "Config path not found, generate at: " <<  q_string_value << "\n";
-      #endif
-        // clang-format on
+		case Status::READ_FILE_FAILED:
+			QMessageBox::critical(main_window, "Error",
+								  "Could not read configuration file");
+			break;
 
-        auto status = ffi::write_default_config();
-        on_error(status, main_window);
-    }
+		case Status::UTF_8_ERROR:
+			QMessageBox::critical(main_window, "Error",
+								  "UTF-8 error in configuration file");
+			break;
+	}
+}
 
-    // clang-format off
-    #if defined (LAZY_DEBUG)
-      auto q_string_value = QString::fromStdString(config_path);
+string Self::application_config() {
+	auto config_dir_raw = ffi::config_dir();
 
-      qDebug() << "Found config path at: " << q_string_value << "\n";
-    #endif
-    // clang-format on
+	stringstream string_stream;
+	string_stream << config_dir_raw << "/Lazyboard"
+				  << "/settings.toml";
+	string config_path = string_stream.str();
+
+	ffi::free_c_str(config_dir_raw);
+	return config_path;
+}
+
+void Self::create_default_config(QMainWindow *main_window) {
+	auto config_path = this->application_config();
+	auto is_config_exists = ffi::is_exists_path(config_path.data());
+
+	if (!is_config_exists) {
+		auto status = ffi::write_default_config();
+		on_gen_default_cfg_error(status, main_window);
+
+		// clang-format off
+		#if defined(LAZY_DEBUG)
+				cout << "[DEBUG] " << "Config path not found, generate at: " << config_path
+					<< "\n";
+		#endif
+
+		return;
+	}
+
+	#if defined(LAZY_DEBUG)
+		cout << "[DEBUG] " <<"Found config path at:" << config_path << "\n";
+	#endif	// clang-format on
+}
+
+void Self::set_application_theme(QMainWindow *main_window,
+								 RawAppConfig *app_config) {
+	auto bg_color = string(app_config->raw_app_gui_settings.background_color);
+	auto fg_color = string(app_config->raw_app_gui_settings.foreground_color);
+
+	auto bg_hex = QColor(QString::fromStdString(bg_color));
+	auto fg_hex = QColor(QString::fromStdString(fg_color));
+	free_cstr_app_config(raw_app_config.get());
+
+	if (!bg_hex.isValid() || !fg_hex.isValid()) {
+		stringstream string_stream;
+
+		string_stream << "Invalid HEX color, please check your configuration\n"
+					  << "Your 'background_color' setting: " << bg_color << "\n"
+					  << "Your 'foregounrd_color: setting: " << fg_color
+					  << "\n";
+		auto error_message = string_stream.str();
+
+		QMessageBox::critical(main_window, "Error", error_message.c_str());
+		// clang-format off
+		#if defined (LAZY_DEBUG)
+			stringstream string_stream_debug;
+			string_stream_debug  
+				<< "[DEBUG] Found error when load TOML configuration:\n"
+				<< "[DEBUG] Error Type: 'Invalid HEX Color'\n"
+				<< "[DEBUG] 'bg_color' setting: " << bg_color << "\n"
+				<< "[DEBUG] 'fg_color' setting: " << fg_color << "\n";
+
+			cout << string_stream_debug.str().c_str();
+
+		#endif	// clang-format on
+
+		return;
+	}
+
+	QPalette palette;
+
+	palette.setColor(QPalette::Window, bg_hex);
+	palette.setColor(QPalette::Base, bg_hex);
+	palette.setColor(QPalette::WindowText, fg_hex);
+
+	main_window->setPalette(palette);
+	main_window->setAutoFillBackground(true);
+}
+
+void Self::read_if_exists_config(QMainWindow *main_window) {
+	raw_app_config = make_unique<RawAppConfig>();
+	auto config_path = this->application_config();
+
+	auto status = exists_config(config_path.data(), raw_app_config.get());
+	this->on_read_exists_cfg_error(status, main_window);
+
+	// clang-format off
+	#if defined(LAZY_DEBUG)
+		cout << "[DEBUG] " << "Found config setting 'background_color': "
+			<< raw_app_config->raw_app_gui_settings.background_color << "\n";
+
+		cout << "[DEBUG] " << "Found config setting 'foreground_color': "
+			<< raw_app_config->raw_app_gui_settings.foreground_color << "\n";
+	#endif	// clang-format on
+
+	this->set_application_theme(main_window, raw_app_config.get());
 }
