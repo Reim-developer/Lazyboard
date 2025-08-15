fe73f6f3dcde96be28b497f18a869c24c422407f
Reim-developer <contact.kaxtr@gmail.com>
2025-08-15 23:04:18 +0700

    Implement `background_button_color` & `foreground_button_color` load to GUI

--- src/back_end/src/raw_config/raw_toml.rs
+++ src/back_end/src/raw_config/raw_toml.rs
@@ -1,3 +1,10 @@
+use crate::raw_config::constant::{
+    APP_GUI_SETTINGS, APP_SETTINGS, BACKGROUND_COLOR, BACKGROUND_COLOR_BUTTON,
+    FALLBACK_BG_BUTTON_COLOR, FALLBACK_BG_COLOR, FALLBACK_FG_BUTTON_COLOR,
+    FALLBACK_FG_COLOR, FOREGROUND_COLOR, FOREGROUND_COLOR_BUTTON,
+    HIDE_WHEN_CLOSED, HIDE_WHEN_CLOSED_FALLBACK, NOTIFICATION,
+    NOTIFICATION_FALLBACK,
+};
 use std::{
     ffi::{CStr, CString, c_char},
     fs,
@@ -14,6 +21,8 @@ pub struct RawAppSettings {
 pub struct RawAppGuiSettings {
     pub background_color: *mut c_char,
     pub foreground_color: *mut c_char,
+    pub background_button_color: *mut c_char,
+    pub foreground_button_color: *mut c_char,
 }
 
 #[repr(C)]
@@ -29,10 +38,38 @@ pub enum RawReadAppConfigStatus {
     Utf8Error,
     ParseTomlFailed,
     ConvertToMutFailed,
+    ConvertToCStringFailed,
 }
 
-const FALLBACK_BG_COLOR: &str = "#2f3136";
-const FALLBACK_FG_COLOR: &str = "#ffffff";
+fn color_by_settings(
+    toml_value: &Value,
+    field_name: &str,
+    fallback: &str,
+) -> String {
+    let color = toml_value
+        .get(APP_GUI_SETTINGS)
+        .and_then(|value| value.get(field_name))
+        .and_then(Value::as_str)
+        .unwrap_or(fallback);
+
+    color.to_string()
+}
+
+fn raw_app_settings(toml_value: &Value) -> RawAppSettings {
+    RawAppSettings {
+        hide_when_closed: toml_value
+            .get(APP_SETTINGS)
+            .and_then(|value| value.get(HIDE_WHEN_CLOSED))
+            .and_then(Value::as_bool)
+            .unwrap_or(HIDE_WHEN_CLOSED_FALLBACK),
+
+        notification: toml_value
+            .get(APP_SETTINGS)
+            .and_then(|value| value.get(NOTIFICATION))
+            .and_then(Value::as_bool)
+            .unwrap_or(NOTIFICATION_FALLBACK),
+    }
+}
 
 /// # Safety
 /// Careful with raw pointers.
@@ -56,38 +93,37 @@ pub unsafe extern "C" fn raw_exists_config(
         return Status::ParseTomlFailed;
     };
 
-    let app_settings = RawAppSettings {
-        hide_when_closed: toml_value
-            .get("app_settings")
-            .and_then(|value| value.get("hide_when_closed"))
-            .and_then(Value::as_bool)
-            .unwrap_or(false),
+    let app_settings = raw_app_settings(&toml_value);
 
-        notification: toml_value
-            .get("app_settings")
-            .and_then(|value| value.get("notification"))
-            .and_then(Value::as_bool)
-            .unwrap_or(false),
-    };
-
-    let bg_color = toml_value
-        .get("app_gui_settings")
-        .and_then(|value| value.get("background_color"))
-        .and_then(Value::as_str)
-        .unwrap_or(FALLBACK_BG_COLOR);
-
-    let fg_color = toml_value
-        .get("app_gui_settings")
-        .and_then(|value| value.get("foreground_color"))
-        .and_then(Value::as_str)
-        .unwrap_or(FALLBACK_FG_COLOR);
+    let bg_color =
+        color_by_settings(&toml_value, BACKGROUND_COLOR, FALLBACK_BG_COLOR);
+    let fg_color =
+        color_by_settings(&toml_value, FOREGROUND_COLOR, FALLBACK_FG_COLOR);
+    let bg_button_color = color_by_settings(
+        &toml_value,
+        BACKGROUND_COLOR_BUTTON,
+        FALLBACK_BG_BUTTON_COLOR,
+    );
+    let fg_button_color = color_by_settings(
+        &toml_value,
+        FOREGROUND_COLOR_BUTTON,
+        FALLBACK_FG_BUTTON_COLOR,
+    );
 
     let Ok(bg_cstr) = CString::new(bg_color) else {
-        return Status::ParseTomlFailed;
+        return Status::ConvertToCStringFailed;
     };
 
     let Ok(fg_cstr) = CString::new(fg_color) else {
-        return Status::ParseTomlFailed;
+        return Status::ConvertToCStringFailed;
+    };
+
+    let Ok(bg_btn_cstr) = CString::new(bg_button_color) else {
+        return Status::ConvertToCStringFailed;
+    };
+
+    let Ok(fg_btn_cstr) = CString::new(fg_button_color) else {
+        return Status::ConvertToCStringFailed;
     };
 
     if let Some(config) = unsafe { raw_cfg_out.as_mut() } {
@@ -95,6 +131,10 @@ pub unsafe extern "C" fn raw_exists_config(
         config.app_settings.notification = app_settings.notification;
         config.app_gui_settings.background_color = bg_cstr.into_raw();
         config.app_gui_settings.foreground_color = fg_cstr.into_raw();
+        config.app_gui_settings.background_button_color =
+            bg_btn_cstr.into_raw();
+        config.app_gui_settings.foreground_button_color =
+            fg_btn_cstr.into_raw();
 
         return RawReadAppConfigStatus::Ok;
     }
@@ -110,6 +150,10 @@ pub unsafe extern "C" fn raw_free_cstr_app_config(config: *mut RawAppConfig) {
         if let Some(cfg) = config.as_mut() {
             let _ = CString::from_raw(cfg.app_gui_settings.background_color);
             let _ = CString::from_raw(cfg.app_gui_settings.foreground_color);
+            let _ =
+                CString::from_raw(cfg.app_gui_settings.background_button_color);
+            let _ =
+                CString::from_raw(cfg.app_gui_settings.foreground_button_color);
         }
     }
 }
