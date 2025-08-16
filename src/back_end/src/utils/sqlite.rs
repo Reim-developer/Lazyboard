d2e949089545c1edbb42487f24ab640f8872ed6e
Reim-developer <contact.kaxtr@gmail.com>
2025-08-16 19:44:04 +0700

    implement `raw_init_clipboard_cache` function & SQL query execute

---
+++ src/back_end/src/utils/sqlite.rs
@@ -0,0 +1,41 @@
+use sqlite::Connection;
+use std::ffi::{CStr, c_char};
+
+#[repr(C)]
+pub enum InitDatabaseStatus {
+    Ok,
+    CreateDatabaseFailed,
+    CStrConvertFailed,
+    ExecuteSqlFailed,
+}
+
+const INIT_QUERY: &str = r"--sql
+	CREATE TABLE IF NOT EXISTS clipboard_cache
+";
+
+#[unsafe(no_mangle)]
+/// # Safety
+/// Careful with unsafe context & raw pointers.
+pub unsafe extern "C" fn raw_init_clipboard_cache(
+    file_path: *const c_char,
+) -> InitDatabaseStatus {
+    unsafe {
+        use InitDatabaseStatus as Status;
+
+        let Ok(file_path_cstr) = CStr::from_ptr(file_path).to_str() else {
+            return Status::CStrConvertFailed;
+        };
+
+        let Ok(sql) = Connection::open(file_path_cstr) else {
+            return Status::CreateDatabaseFailed;
+        };
+
+        let is_err = sql.execute(INIT_QUERY).is_err();
+
+        if is_err {
+            return Status::ExecuteSqlFailed;
+        }
+
+        Status::Ok
+    }
+}
