af162b98a5fe393acf397e3acb8bcfc62bf6e13a
Reim-developer <contact.kaxtr@gmail.com>
2025-08-16 10:32:32 +0700

    added constant value

--- src/back_end/src/raw_config/constant.rs
+++ src/back_end/src/raw_config/constant.rs
@@ -13,9 +13,13 @@ pub const BACKGROUND_COLOR: &str = "background_color";
 pub const FOREGROUND_COLOR: &str = "foreground_color";
 pub const BACKGROUND_COLOR_BUTTON: &str = "background_button_color";
 pub const FOREGROUND_COLOR_BUTTON: &str = "fore_background_color";
+pub const BACKGROUND_COLOR_TABLE_HEADER: &str = "background_table_header_color";
+pub const FOREGROUND_COLOR_TABLE_HEADER: &str = "foreground_table_header_color";
 
 /* Default application color fallback */
 pub const FALLBACK_BG_COLOR: &str = "#2f3136";
 pub const FALLBACK_FG_COLOR: &str = "#ffffff";
 pub const FALLBACK_BG_BUTTON_COLOR: &str = "#2f3136";
 pub const FALLBACK_FG_BUTTON_COLOR: &str = "#ffffff";
+pub const FALLBACK_BG_TABLE_HEADER_COLOR: &str = "#2f3136";
+pub const FALLBACK_FG_TABLE_HEADER_COLOR: &str = "#ffffff";
