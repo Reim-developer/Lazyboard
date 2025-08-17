0fadf996fe19a04e881a88ec6f29b0726490a267
Reim-developer <contact.kaxtr@gmail.com>
2025-08-17 22:11:36 +0700

    Implement `SQLite` Manager for Application

---
+++ src/front_end_db/include/sqlite_manager.hxx
@@ -0,0 +1,25 @@
+#ifndef SQLITE_MANAGER_HXX
+#define SQLITE_MANAGER_HXX
+#include <qmainwindow.h>
+
+#include "../../ffi/raw/sqlite.hxx"
+#include "../../ffi/raw/utils.hxx"
+
+namespace Lazyboard::front_end_db {
+class SQLiteManager {
+   private:
+	using InitDataResult = InitDatabaseStatus;
+	using MkdirResult = CreateFileSystemStatus;
+
+	void on_create_folder_error(const MkdirResult &result) noexcept;
+	void on_create_clipboard_cache_error(const InitDataResult &status) noexcept;
+
+   public:
+	void create_clipboard_cache(QMainWindow *main_window) noexcept;
+
+   private:
+	QMainWindow *_main_window;
+};
+}  // namespace Lazyboard::front_end_db
+
+#endif	// SQLITE_MANAGER_HXX
