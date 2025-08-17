e461f09e0fd6838fad09f4140606a2817b5c1e6b
Reim-developer <contact.kaxtr@gmail.com>
2025-08-17 17:03:57 +0700

    Defined `errror_dialog_show` & add `noexcept` to some utils function

--- src/front_end_utils/include/utils.hxx
+++ src/front_end_utils/include/utils.hxx
@@ -2,9 +2,12 @@
 #define FRONT_END_UTILS_HXX
 
 #include <qcolor.h>
+#include <qcontainerfwd.h>
 #include <qicon.h>
+#include <qmessagebox.h>
 #include <qpixmap.h>
 #include <qstringview.h>
+#include <qwidget.h>
 
 #include <initializer_list>
 #include <type_traits>
@@ -37,7 +40,7 @@ inline constexpr bool is_valid_hex_color(const Args&... args) noexcept {
 		...);
 }
 
-inline QIcon image_from_bytes(const initializer_list<uint8_t>& data) {
+inline QIcon image_from_bytes(const initializer_list<uint8_t>& data) noexcept {
 	QByteArray bytes_array;
 	bytes_array.reserve(static_cast<int>(data.size()));
 
@@ -51,6 +54,14 @@ inline QIcon image_from_bytes(const initializer_list<uint8_t>& data) {
 	}
 
 	return QIcon();
+}
+
+inline void error_dialog_show(QWidget* parent, const QString& message,
+							  ErrorTypes error_types) noexcept {
+	auto error_string = error_to_string(error_types).data();
+	auto fmt_msg = QString("%1\nError name: %2").arg(message).arg(error_string);
+
+	QMessageBox::critical(parent, "Error", fmt_msg);
 }
 
 }  // namespace Lazyboard::front_end_utils
