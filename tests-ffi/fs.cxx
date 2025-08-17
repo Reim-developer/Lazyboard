a3786d35d4fa959528c75ce95e34c66008a3637a
Reim-developer <contact.kaxtr@gmail.com>
2025-08-17 13:03:21 +0700

    Implement FFI test for Rust backend function `raw_cache_dir`

--- tests-ffi/fs.cxx
+++ tests-ffi/fs.cxx
@@ -1,13 +1,33 @@
-
 #include <cassert>
-extern "C" bool raw_is_path_exists(const char *path);
+#include <cstddef>
+#include <cstdlib>
+#include <span>
+#include <string>
 
-int main() {
-	const char *valid_path = "../tests-ffi";
+using std::span;
+using std::string;
+
+extern "C" bool raw_is_path_exists(const char *path);
+extern "C" char *raw_cache_dir();
+extern "C" void raw_free_c_str(char *c_str);
+
+int main(int argc, char *argv[]) {
+	const auto this_file_span = span(argv, size_t(argc));
+
+	if (this_file_span.empty()) {
+		abort();
+	}
+
+	const char *file_name = this_file_span[0];
 	const char *wrong_path = "../abcxyzw1133";
 
-	assert(raw_is_path_exists(valid_path));
+	char *raw_cache_dir_value = raw_cache_dir();
+	const string cache_dir = string(raw_cache_dir_value);
+	raw_free_c_str(raw_cache_dir_value);
+
+	assert(raw_is_path_exists(file_name));
 	assert(!raw_is_path_exists(wrong_path));
+	assert(!cache_dir.empty());
 
 	return 0;
 }
