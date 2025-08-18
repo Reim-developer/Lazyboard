7d2ad021482ed08899bd07af58e62016a37a4af6
Reim-developer <contact.kaxtr@gmail.com>
2025-08-18 22:12:15 +0700

    Improve error handling & debug info: + New function `debug_info`

--- src/front_end_utils/include/utils.hxx
+++ src/front_end_utils/include/utils.hxx
@@ -9,13 +9,17 @@
 #include <qstringview.h>
 #include <qwidget.h>
 
+#include <algorithm>
 #include <initializer_list>
 #include <type_traits>
+#include <utility>
 #include <vector>
 
 #include "error_types.hxx"
 
 using std::abort;
+using std::for_each;
+using std::forward;
 using std::initializer_list;
 using std::is_same_v;
 using std::stringstream;
@@ -29,15 +33,14 @@ using std::source_location;
 #endif
 
 namespace Lazyboard::front_end_utils {
-
 template <typename T>
 concept is_qcolor = requires(const T& t) { QColor(t); };
 
 template <is_qcolor... Args>
-inline constexpr bool is_valid_hex_color(const Args&... args) noexcept {
+inline constexpr bool is_valid_hex_color(Args&&... args) noexcept {
 	return (
 		[&]() {
-			QColor color(args);
+			QColor color(std::forward<Args>(args));
 
 			if (!color.isValid()) {
 				return false;
@@ -49,13 +52,13 @@ inline constexpr bool is_valid_hex_color(const Args&... args) noexcept {
 }
 
 using init_list = initializer_list<uint8_t>;
-inline QIcon image_from_bytes(const init_list& data) noexcept {
+inline QIcon image_from_bytes(const init_list&& data) noexcept {
 	QByteArray bytes_array;
 	bytes_array.reserve(static_cast<int>(data.size()));
 
-	for (auto byte : data) {
-		bytes_array.append(static_cast<char>(byte));
-	}
+	for_each(data.begin(), data.end(), [&bytes_array](auto bytes) {
+		bytes_array.append(static_cast<char>(bytes));
+	});
 
 	QPixmap pixmap;
 	if (pixmap.loadFromData(bytes_array)) {
@@ -74,13 +77,13 @@ inline void error_dialog_show(QWidget* parent,
 }
 
 #if defined(LAZY_DEBUG)
+const auto green = "\x1b[32m";
+const auto white = "\x1b[37m";
+
 using src_loc = source_location;
 template <typename T>
 inline void dump_ptr_address(T* t,
 							 const src_loc& location = src_loc::current()) {
-	auto green = "\x1b[32m";
-	auto white = "\x1b[37m";
-
 	auto file_name = location.file_name();
 	auto file_line = location.line();
 
@@ -88,6 +91,17 @@ inline void dump_ptr_address(T* t,
 	println("{}[MEMORY_DEBUG]{} Line: {}", green, white, file_line);
 	println("{}[MEMORY_DEBUG]{} {}", green, white, static_cast<void*>(t));
 }
+
+template <typename T>
+concept string_bound = requires(const T& t) { string(t); };
+
+template <typename string_bound>
+inline void debug_info(string_bound&& information) {
+	auto info = forward<string_bound>(information);
+
+	println("{}[INFO_DEBUG]{} {}", green, white, info);
+}
+
 #endif
 
 }  // namespace Lazyboard::front_end_utils
