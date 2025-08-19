42a44f9aab7943fc0cad5d3fd1a75c90b8c16eae
Reim-developer <contact.kaxtr@gmail.com>
2025-08-19 16:05:04 +0700

    Improve test: added unit test in FFI side for `raw_add_text_clipboard`

--- tests-ffi/sqlite.cxx
+++ tests-ffi/sqlite.cxx
@@ -1,7 +1,9 @@
 
 #include <cassert>
 #include <cstdint>
+#include <filesystem>
 using std::uint8_t;
+using std::filesystem::remove;
 
 extern "C" enum class QueryResult : uint8_t {
 	OK,
@@ -10,18 +12,42 @@ extern "C" enum class QueryResult : uint8_t {
 	EXECUTE_SQL_ERR
 };
 
+extern "C" struct TextClipboard {
+	const char* content;
+	const char* content_hash;
+	const char* content_type;
+	bool is_pinned;
+};
+
 extern "C" auto raw_init_clipboard_cache(const char* path) -> QueryResult;
+extern "C" QueryResult raw_add_text_clipboard(const char* path,
+											  TextClipboard text_clipboard);
 
 int main() {
 	using R = QueryResult;
 
 	const char* test_path = "test_init.db";
-	auto status = raw_init_clipboard_cache(test_path);
+	remove(test_path);
 
-	assert(status != R::OPEN_DATABASE_ERR);
-	assert(status != R::C_STR_CONVERT_ERR);
-	assert(status != R::EXECUTE_SQL_ERR);
-	assert(status == R::OK);
+	auto text_clipboard = TextClipboard{
+		.content = "test",
+		.content_hash = "test_hash",
+		.content_type = "text",
+		.is_pinned = true,
+	};
+
+	auto init_result = raw_init_clipboard_cache(test_path);
+	auto add_text_result = raw_add_text_clipboard(test_path, text_clipboard);
+
+	assert(init_result != R::OPEN_DATABASE_ERR);
+	assert(init_result != R::C_STR_CONVERT_ERR);
+	assert(init_result != R::EXECUTE_SQL_ERR);
+	assert(init_result == R::OK);
+
+	assert(add_text_result != R::C_STR_CONVERT_ERR);
+	assert(add_text_result != R::EXECUTE_SQL_ERR);
+	assert(add_text_result != R::OPEN_DATABASE_ERR);
+	assert(add_text_result == R::OK);
 
 	return 0;
 }
